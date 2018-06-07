d92b88b23679e713dc18642cb1174714bffa01d9
ycr0701fgtalyc <39934766+ycr0701fgtalyc@users.noreply.github.com>
2018-06-07 11:12:26 +0800

    committee member and standby struct

--- true/committee.go
+++ true/committee.go
@@ -1,1 +1,20 @@
+package true
 
+package main
+
+//"strings"
+
+type CommitteeMember struct {
+	Nodeid string
+	Addr   string
+	Port   int
+}
+
+type StandbyInfo struct {
+	NodeId   string
+	coinbase string
+	addr     string
+	port     int
+	height   *big.Int
+	comfire  bool
+}
