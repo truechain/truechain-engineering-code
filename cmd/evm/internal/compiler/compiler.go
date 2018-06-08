3d8aa9d64e421b9e51a0fdb3db4905e0317c6c44
sdwp111 <you@example.com>
2018-06-08 10:40:19 +0800

    Transaction pool code update

--- cmd/evm/internal/compiler/compiler.go
+++ cmd/evm/internal/compiler/compiler.go
@@ -28,6 +28,7 @@ func Compile(fn string, src []byte, debug bool) (string, error) {
 	compiler := asm.NewCompiler(debug)
 	compiler.Feed(asm.Lex(fn, src, debug))
 
+	
 	bin, compileErrors := compiler.Compile()
 	if len(compileErrors) > 0 {
 		// report errors
