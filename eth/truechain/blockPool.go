fbf7a4e76416a0ac5e81000439fdeed3c82a60e4
shijinyuan <13152720003@163.com>
2018-06-19 14:10:12 +0800

    update blockpool

--- eth/truechain/blockPool.go
+++ eth/truechain/blockPool.go
@@ -3,9 +3,9 @@ package truechain
 import (
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
+	"math/big"
+	"github.com/ethereum/go-ethereum/common"
 )
-
-
 
 
 type BlockPool struct {
@@ -16,43 +16,17 @@ type BlockPool struct {
 	th				*TrueHybrid
 }
 
+
 type NewBlocksEvent struct {block *TruePbftBlock}
+
 
 
 //添加块
 func (self *BlockPool) addBlock(block *TruePbftBlock) {
 
-
 	//判断是否委员
-	self.th.CheckBlock(block)
-
-	//
-	txs := make([]*types.Transaction,0,0)
-
-	//txs := make([]*Transaction,0,0)
-
-	for _,v := range block.Txs.Txs {
-
-		to := make([]byte,0,0)
-		txData := v.GetData()
-
-		if t := txData.GetRecipient(); t != nil {
-			//to = t.Bytes()
-		}
-
-		txs = append(txs,&types.Transaction{
-			data:&types.txdata{
-				AccountNonce:   	txData.GetAccountNonce(),
-				price:              txData.GetPrice(),
-				GasLimit:           txData.GetGasLimit(),
-				Recipient:          to,
-				Amount	:           txData.GetAmount(),
-				Payload:            txData.GetPayload(),
-				V:                  txData.GetV(),
-				R:                  txData.GetR(),
-				S:                  txData.GetS(),
-			},
-		})
+	if(self.th.CheckBlock(block)==nil){
+		self.blocks = append(self.blocks, block)
 	}
 
 
@@ -60,43 +34,39 @@ func (self *BlockPool) addBlock(block *TruePbftBlock) {
 }
 
 
+//接入到原来以太的pow挖矿流程
+func (self *BlockPool) joinEth() {
+
+	for{
+
+		for _,block := range self.blocks {
+
+			txs := make([]*types.Transaction,0,0)
 
 
-//func (self *BlockPool) wait()  {
-//
-//	for {
-//		// A real event arrived, process interesting content
-//		select {
-//			// Handle ChainHeadEvent
-//
-//			case bc :=<- self.blockCh:
-//				//验证自身是否委员
-//
-//				//验证是否委员会发出
-//				self.th.CheckBlock(bc.block)
-//
-//				txs := [len(bc.block.Txs.Txs)] truechain.Transaction{}
-//
-//				for tx := range bc.block.Txs.Txs {
-//
-//
-//
-//					true_tx := truechain.Transaction
-//
-//					true_tx.
-//
-//
-//				}
-//
-//
-//
-//
-//
-//
-//
-//		}
-//	}
-//
-//
-//
-//}
+			//转换交易
+			for _,v := range block.Txs.Txs {
+
+				txData := v.GetData()
+				//nonce uint64, to common.Address, amount *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte
+				var to common.Address
+				to.SetBytes(txData.GetRecipient())
+				//创建新的交易
+				transaction := types.NewTransaction(txData.GetAccountNonce(),to,big.NewInt(txData.GetAmount()),uint64(txData.GetGasLimit()),nil,txData.GetPayload())
+				txs = append(txs,transaction)
+			}
+
+			//将交易传回
+			self.txsCh <- core.NewTxsEvent{Txs:txs}
+
+		}
+
+
+
+
+	}
+
+
+
+}
+
