2260cab6e190196215d6b532f91c074452fb0298
sdwp111 <37368590+sdwp111@users.noreply.github.com>
2018-06-27 22:22:17 +0800

    Update bytes.go

--- common/bytes.go
+++ common/bytes.go
@@ -15,6 +15,7 @@
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 // Package common contains various helper functions.
+
 package common
 
 import "encoding/hex"
