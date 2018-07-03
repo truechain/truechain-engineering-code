b31c32921f67a94073402b75ba1eefc0c894c84c
chore <894078074@qq.com>
2018-07-03 18:18:46 +0800

    pbft update

--- eth/pbft.go
+++ eth/pbft.go
@@ -2,9 +2,11 @@ package eth
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
 	"github.com/ethereum/go-ethereum/eth/truechain"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 //type PbftPool interface {
@@ -22,14 +24,7 @@ type newBftBlockData struct {
 //func (b *truechain.TruePbftBlock) hash() atomic.Value {
 //	return atomic.Value{}
 //}
-//
-//func prlpHash(x interface{}) (h common.Hash) {
-//	hw := sha3.NewKeccak256()
-//	rlp.Encode(hw, x)
-//	hw.Sum(h[:0])
-//	return h
-//}
-//
+
 func (ps *peerSet) PeersWithoutPbftBlock(hash common.Hash) []*peer {
 	ps.lock.RLock()
 	defer ps.lock.RUnlock()
@@ -41,20 +36,42 @@ func (ps *peerSet) PeersWithoutPbftBlock(hash common.Hash) []*peer {
 	}
 	return list
 }
+//func (ps *peerSet) PeersWithoutPbftCms(hash common.Hash) []*peer {
+//	ps.lock.RLock()
+//	defer ps.lock.RUnlock()
+//	list := make([]*peer, 0, len(ps.peers))
+//	for _, p := range ps.peers {
+//		if !p.knownBftCms.Has(hash) {
+//			list = append(list, p)
+//		}
+//	}
+//	return list
+//}
+//func (ps *peerSet) PeersWithoutPbftCds(hash common.Hash) []*peer {
+//	ps.lock.RLock()
+//	defer ps.lock.RUnlock()
+//	list := make([]*peer, 0, len(ps.peers))
+//	for _, p := range ps.peers {
+//		if !p.knownBftCds.Has(hash) {
+//			list = append(list, p)
+//		}
+//	}
+//	return list
+//}
 
 //
 //func (h truechain.TruePbftBlockHeader) Hash() common.Hash {
 //	return prlpHash(h)
 //}
-//func (h CMS) Hash() common.Hash {
-//	return prlpHash(h)
-//}
-//func (h SBS) Hash() common.Hash {
-//	return prlpHash(h)
-//}
-//
-//type CMS []*truechain.CommitteeMember
-//type SBS []*truechain.CommitteeMember
+func (h CMS) Hash() common.Hash {
+	return prlpHash(h)
+}
+func (h CDS) Hash() common.Hash {
+	return prlpHash(h)
+}
+
+type CMS []*truechain.PbftCommittee
+type CDS []*truechain.PbftCdCommittee
 
 //bpft
 type propBftEvent struct {
@@ -62,11 +79,11 @@ type propBftEvent struct {
 }
 
 //type propCMSEvnet struct {
-//	cms []*truechain.CommitteeMember
+//	cms []*truechain.PbftCommittee
 //}
 //
-//type propSBSEvent struct {
-//	oms []*truechain.CommitteeMember
+//type propCDSEvent struct {
+//	cds []*truechain.PbftCdCommittee
 //}
 
 func (p *peer) SendNewBftBlock(b *truechain.TruePbftBlock) error {
@@ -87,34 +104,34 @@ func (p *peer) AsyncSendNewBftBlocks(blocks []*truechain.TruePbftBlock) {
 	}
 }
 
-////cms
-//
-//func (p *peer) SendCMS(cms CMS) error {
-//	p.knownCurCmms.Add(cms.Hash())
-//	return p2p.Send(p.rw, NewCMSMsg, []interface{}{cms})
-//}
-//
+//cms
+
+func (p *peer) SendCMS(cms CMS) error {
+	p.knownBftCms.Add(cms.Hash())
+	return p2p.Send(p.rw, CMSMsg, []interface{}{cms})
+}
+
 //func (p *peer) AsyncCMS(cms CMS) {
 //	select {
-//	case p.queuedCms <- &propCMSEvnet{cms: cms}:
-//		p.knownCurCmms.Add(cms.Hash())
+//	case p.queuedCmsProps <- cms:
+//		p.knownBftCms.Add(cms.Hash())
 //	default:
 //		p.Log().Debug("Dropping cms propagation", "hash", cms.Hash())
 //	}
 //}
-//
-////oms
-//func (p *peer) SendSBS(sbs SBS) error {
-//	p.knownCurCmms.Add(sbs.Hash())
-//	return p2p.Send(p.rw, NewCMSMsg, []interface{}{sbs})
-//}
-//
-//func (p *peer) AsyncSBS(oms SBS) {
+
+//oms
+func (p *peer) SendCDS(cds CDS) error {
+	p.knownBftCds.Add(cds.Hash())
+	return p2p.Send(p.rw, CDSMsg, []interface{}{cds})
+}
+
+//func (p *peer) AsyncCDS(cds CDS) {
 //	select {
-//	case p.queuedSbs <- &propSBSEvent{oms: oms}:
-//		p.knownBftBlocks.Add(oms.Hash())
+//	case p.queuedCDsProps <- cds:
+//		p.knownBftBlocks.Add(cds.Hash())
 //	default:
-//		p.Log().Debug("Dropping oms propagation", "hash", oms.Hash())
+//		p.Log().Debug("Dropping oms propagation", "hash", cds.Hash())
 //	}
 //}
 
@@ -132,8 +149,8 @@ func (pm *ProtocolManager) pbBroadcastloop() {
 		select {
 		case event := <-pm.pblocksCh:
 			pm.BroadcastPbs(event)
-		//case <-pm.pbsSub.Err():
-		//	return
+			//case <-pm.pbsSub.Err():
+			//	return
 		}
 	}
 }
@@ -152,9 +169,38 @@ func (pm *ProtocolManager) BroadcastPbs(pbs []*truechain.TruePbftBlock) {
 	}
 }
 
-func (p *peer) SendCms(cms []*truechain.CommitteeMember) error {
-	return p2p.Send(p.rw, MainMumbersMsg, cms)
-}
-func (p *peer) SendCds(cms []*truechain.CommitteeMember) error {
-	return p2p.Send(p.rw, CDSMsg, cms)
+//func (p *peer) SendCms(cms []*truechain.PbftCommittee) error {
+//	return p2p.Send(p.rw, CMSMsg, cms)
+//}
+//func (p *peer) SendCds(cds []*truechain.PbftCdCommittee) error {
+//	return p2p.Send(p.rw, CDSMsg, cds)
+//}
+
+//func (pm *ProtocolManager) BroadcastCms(cms []*truechain.PbftCommittee) {
+//	var pbset = make(map[*peer][]*truechain.PbftCommittee)
+//	peers := pm.peers.PeersWithoutPbftCms(cms.Hash())
+//	for _, peer := range peers {
+//		pbset[peer] = append(pbset[peer], pb)
+//	}
+//	log.Trace("Broadcast cms", "hash", cms.Hash())
+//	peer.AsyncSendNewBftBlocks(pbs)
+//}
+//func (pm *ProtocolManager) BroadcastCds(pbs []*truechain.CommitteeMember) {
+//	var pbset = make(map[*peer][]*truechain.CommitteeMember)
+//	for _, pb := range pbs {
+//		peers := pm.peers.PeersWithoutPbftCds(pb.Hash())
+//		for _, peer := range peers {
+//			pbset[peer] = append(pbset[peer], pb)
+//		}
+//		log.Trace("Broadcast pbftblcok", "hash", pb.Hash())
+//	}
+//	for peer, pbs := range pbset {
+//		peer.AsyncSendNewBftBlocks(pbs)
+//	}
+//}
+func prlpHash(x interface{}) (h common.Hash) {
+	hw := sha3.NewKeccak256()
+	rlp.Encode(hw, x)
+	hw.Sum(h[:0])
+	return h
 }
