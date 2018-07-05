a51bb7cf4bdfc9d6a02f02cd397360b88d7eef63
paulakisscat <33856253+paulakisscat@users.noreply.github.com>
2018-07-05 18:54:23 +0800

    Update server.js

--- tests/server.js
+++ tests/server.js
@@ -21,7 +21,7 @@ web3.eth.personal.unlockAccount(FROM_ADDRESS, PASSWORD).then(async success => {
   }
   // const nonceBefore = await web3.eth.getTransactionCount(FROM_ADDRESS, 'pending')
   // console.log(`nonce before test: ${nonceBefore}`)
-  const time = new Date()
+  const time = new Dat()
   for (let i = 0; i < TX_COUNT; i++) {
     await sleep(2)
     web3.eth.sendTransaction({
