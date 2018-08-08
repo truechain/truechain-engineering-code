2e8052225f66b3c969536ad5469642776ce674c9
wbqlove <wbqlove1234@163.com>
2018-08-08 11:43:55 +0800

    etrue/election fix VerifyFastBlockSigns

--- etrue/election.go
+++ etrue/election.go
@@ -2,78 +2,75 @@ package etrue
 
 import (
 	"crypto/ecdsa"
+	"crypto/sha256"
+	"github.com/truechain/truechain-engineering-code/common"
+	"github.com/truechain/truechain-engineering-code/core"
+	"github.com/truechain/truechain-engineering-code/core/fastchain"
 	"github.com/truechain/truechain-engineering-code/core/types"
 	"github.com/truechain/truechain-engineering-code/event"
-	"github.com/truechain/truechain-engineering-code/core"
-	"github.com/truechain/truechain-engineering-code/common"
-	"math/big"
 	"log"
-	"github.com/truechain/truechain-engineering-code/core/fastchain"
-	"crypto/sha256"
-
+	"math/big"
 )
 
-var ( 	z  = 100
- 		k  = 10000
+var (
+	z = 100
+	k = 10000
 )
 
 const (
 	fastChainHeadSize = 256
-	chainHeadSize  = 4096
-
+	chainHeadSize     = 4096
 )
+
 type VoteuUse struct {
-	wi 		int64  //Local value
-	seed 	string
-	b   	bool
-	j 		int
-
+	wi   int64 //Local value
+	seed string
+	b    bool
+	j    int
 }
 
-type Signature struct{
-	blockHash common.Hash
+type Signature struct {
+	blockHash   common.Hash
 	blockNumber *big.Int
-	hash common.Hash
-	sign []byte
+	hash        common.Hash
+	sign        []byte
 }
 
-type queue struct{
+type queue struct {
 	queuesize int   //Array size
-	head int  		//The header and subscript of the queue
-	tail int		//Tail subscript of the queue
-	q []int   		//Array
+	head      int   //The header and subscript of the queue
+	tail      int   //Tail subscript of the queue
+	q         []int //Array
 }
 
-type CommitteeMember struct{
-	ip		[]string
-	port  	uint
+type CommitteeMember struct {
+	ip       []string
+	port     uint
 	coinbase common.Hash
-	pubkey  *ecdsa.PublicKey
+	pubkey   *ecdsa.PublicKey
 }
 
 type Committee struct {
-	Id 	*big.Int
+	Id            *big.Int
 	CommitteeList []*Committee
 }
-
-
 
 type Election struct {
 	genesisCommittee []*CommitteeMember
 
 	committee []*CommitteeMember
-	xh 			uint	//委员会届数
-	fcEvent		fcEvent
-	scEvent		scEvent
-	flag		bool  //用于处理事件切换的标志位
-	number		*big.Int
-	fastHead *big.Int
+	xh        uint //委员会届数
+	fcEvent   fcEvent
+	scEvent   scEvent
+	flag      bool //用于处理事件切换的标志位
+	number    *big.Int
+	fastHead  *big.Int
 	snailHead *big.Int
 
-	CStartFeed		event.Feed
-	CStopFeed		event.Feed
-	EStartFeed		event.Feed
-	scope         event.SubscriptionScope
+	CStartFeed event.Feed
+	CStopFeed  event.Feed
+	EStartFeed event.Feed
+	scope      event.SubscriptionScope
 
 	fastChainHeadCh  chan core.FastChainHeadEvent
 	fastChainHeadSub event.Subscription
@@ -81,17 +78,15 @@ type Election struct {
 	chainHeadCh  chan core.ChainHeadEvent
 	chainHeadSub event.Subscription
 
-	fastchain *fastchain.FastBlockChain
+	fastchain  *fastchain.FastBlockChain
 	snailchain *core.BlockChain
 }
 
-
 //Read creation block information and return public key for signature verification
-func  (v VoteuUse)ReadGenesis()[]string{
+func (v VoteuUse) ReadGenesis() []string {
 
 	return nil
 }
-
 
 //func (e *Election) Events() *event.Feed {
 //	return &e.switchFeed
@@ -109,21 +104,18 @@ type scEvent interface {
 	SubscribeNewChainHeadEvent(chan<- core.ChainHeadEvent) event.Subscription
 }
 
-func (e *Election)start(){
+func (e *Election) start() {
 
-	e.fastChainHeadCh = make(chan core.FastChainHeadEvent, fastChainHeadSize )
+	e.fastChainHeadCh = make(chan core.FastChainHeadEvent, fastChainHeadSize)
 	e.fastChainHeadSub = e.fcEvent.SubscribeNewFastEvent(e.fastChainHeadCh)
 
-
 	e.chainHeadCh = make(chan core.ChainHeadEvent, chainHeadSize)
-	e.chainHeadSub= e.scEvent.SubscribeNewChainHeadEvent(e.chainHeadCh)
+	e.chainHeadSub = e.scEvent.SubscribeNewChainHeadEvent(e.chainHeadCh)
 
 }
 
-
 //Calculate your own force unit locally
-func (v VoteuUse)localForce()int64{
-
+func (v VoteuUse) localForce() int64 {
 
 	w := v.wi
 	//w_i=(D_pf-〖[h]〗_(-k))/u
@@ -146,18 +138,18 @@ func powerf(x float64, n int) float64 {
 }
 
 //Factorial function
-func factorial(){
+func factorial() {
 
 }
 
 //The sum function
-func sigma(j int,k int,wi int,P int64) {
+func sigma(j int, k int, wi int, P int64) {
 
 }
 
 // the draw function is calculated locally for each miner
 // the parameters seed, w_i, W, P are required
-func sortition()bool{
+func sortition() bool {
 	//j := 0;
 	//
 	//for (seed / powerf(2,seedlen)) ^ [Sigma(j,0,wi,P) , Sigma(j+1,0,wi,P)]{
@@ -169,36 +161,33 @@ func sortition()bool{
 	//	}
 	//}
 
-	return false;
-
+	return false
 }
 
 //Used for election counting
-func(qu queue) InitQueue(){
+func (qu queue) InitQueue() {
 
-	qu.queuesize = z;
+	qu.queuesize = z
 
 	//qu.q = make(int,qu.queuesize)
 
-	qu.tail = 0;
-
-	qu.head = 0;
-
+	qu.tail = 0
+	qu.head = 0
 }
 
-func (qu queue) EnQueue(key int){
+func (qu queue) EnQueue(key int) {
 
-	tail := (qu.tail+1) % qu.queuesize //取余保证，当quil=queuesize-1时，再转回0
+	tail := (qu.tail + 1) % qu.queuesize //取余保证，当quil=queuesize-1时，再转回0
 
-	if tail == qu.head{
+	if tail == qu.head {
 
 		log.Print("the queue has been filled full!")
 
 	} else {
 
-	qu.q[qu.tail] = key
+		qu.q[qu.tail] = key
 
-	qu.tail = tail
+		qu.tail = tail
 
 	}
 
@@ -212,7 +201,7 @@ func (qu queue) gettail() int {
 
 // Verify checks a raw ECDSA signature.
 // Returns true if it's valid and false if not.
-func (cm CommitteeMember)Verify(signature []byte)bool {
+func (cm CommitteeMember) Verify(signature []byte) bool {
 	// hash message
 	digest := sha256.Sum256(signature)
 	pubkey := cm.pubkey
@@ -222,26 +211,26 @@ func (cm CommitteeMember)Verify(signature []byte)bool {
 	r.SetBytes(signature[:curveOrderByteSize])
 	s.SetBytes(signature[curveOrderByteSize:])
 
-	return ecdsa.Verify(pubkey,digest[:], r, s)
-
-
+	return ecdsa.Verify(pubkey, digest[:], r, s)
 
 }
+
 //Another method for validation
-func (cm CommitteeMember)ReVerify(FastHeight *big.Int,FastHash common.Hash, ReceiptHash common.Hash, Sign []byte)bool {
+func (cm CommitteeMember) ReVerify(FastHeight *big.Int, FastHash common.Hash, ReceiptHash common.Hash, Sign []byte) bool {
 	return true
 }
+
 //Verify the fast chain committee signatures in batches
-func (cm CommitteeMember) VerifyFastBlockSigns(pvs *[]types.PbftVoteSign) (cfvf *[]types.CommitteeFastSignResult) {
+func VerifyFastBlockSigns(pvs *[]types.PbftVoteSign) (cfvf *[]types.CommitteeFastSignResult) {
 	return cfvf
 }
 
-func (e *Election)elect(FastNumber *big.Int, FastHash common.Hash)[]*CommitteeMember {
+func (e *Election) elect(FastNumber *big.Int, FastHash common.Hash) []*CommitteeMember {
 
 	return nil
 }
 
-func (e *Election)GetCommittee(FastNumber *big.Int, FastHash common.Hash) (*big.Int, []*CommitteeMember){
+func (e *Election) GetCommittee(FastNumber *big.Int, FastHash common.Hash) (*big.Int, []*CommitteeMember) {
 
 	//if block, ok := e.blockCache.Get(hash); ok {
 	//	return nil,block.(*types.FastBlock)
@@ -256,7 +245,7 @@ func (e *Election)GetCommittee(FastNumber *big.Int, FastHash common.Hash) (*big.
 	return nil, nil
 }
 
-func (e *Election)GetXh()uint{
+func (e *Election) GetXh() uint {
 	return e.xh
 }
 
@@ -274,9 +263,9 @@ func (e *Election) loop() {
 			if fb.Block != nil {
 				//Record Numbers to open elections
 				Qu.EnQueue(0)
-				if Qu.gettail() == z-1{
-						//zl := z-13
-						go sortition()
+				if Qu.gettail() == z-1 {
+					//zl := z-13
+					go sortition()
 
 					//bn := fc.GetBlockByNumber(zl)
 
@@ -288,7 +277,7 @@ func (e *Election) loop() {
 			}
 
 		case ev := <-e.fastChainHeadCh:
-			if ev.Block != nil{
+			if ev.Block != nil {
 
 			}
 			if e.flag {
@@ -298,25 +287,22 @@ func (e *Election) loop() {
 	}
 }
 
-
-func NewElction(fastHead *big.Int,snailHead *big.Int,fastchain *fastchain.FastBlockChain,snailchain *core.BlockChain)*Election {
+func NewElction(fastHead *big.Int, snailHead *big.Int, fastchain *fastchain.FastBlockChain, snailchain *core.BlockChain) *Election {
 
 	e := &Election{
-		fastHead:		fastHead,
-		snailHead: 		snailHead,
-		fastchain:		fastchain,
-		snailchain:		snailchain,
-
+		fastHead:   fastHead,
+		snailHead:  snailHead,
+		fastchain:  fastchain,
+		snailchain: snailchain,
 	}
 
 	return e
 }
 
-
 //Interface for some subscribed events
-type CmmitteeStartEvent struct { start  bool}
-type CmmitteeStopEvent struct { stop   bool }
-type ElectionStartEvent struct {election bool}
+type CmmitteeStartEvent struct{ start bool }
+type CmmitteeStopEvent struct{ stop bool }
+type ElectionStartEvent struct{ election bool }
 
 func (e *Election) SubscribeCmmitteeStartEvent(ch chan<- CmmitteeStartEvent) event.Subscription {
 	return e.scope.Track(e.CStopFeed.Subscribe(ch))
@@ -329,4 +315,3 @@ func (e *Election) SubscribeCmmitteeStopEvent(ch chan<- CmmitteeStopEvent) event
 func (e *Election) SubscribeElectionStartEvent(ch chan<- ElectionStartEvent) event.Subscription {
 	return e.scope.Track(e.EStartFeed.Subscribe(ch))
 }
-
