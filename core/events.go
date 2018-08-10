79bac4133cf558c5bb8f82599f45edf915e090fa
shuxunyer@163.com <shuxunyer@163.com>
2018-08-10 10:29:56 +0800

    /core/etrue  add  PbftAgentProxy

--- core/events.go
+++ core/events.go
@@ -89,14 +89,14 @@ type SnailChainSideEvent struct {
 }
 
 type ElectionEvent struct {
-	option 	uint
-	fastnumber	*big.Int
-	snailnumber *big.Int
+	Option 	uint
+	Fastnumber	*big.Int
+	Snailnumber *big.Int
 }
 
 type CommitteeEvent struct {
-	committeeId	*big.Int
-	members []*types.CommitteeMember
+	CommitteeId	*big.Int
+	Members []*types.CommitteeMember
 	}
 type ChainHeadEvent struct{ Block *types.Block }
 type FastChainHeadEvent struct{ Block *types.FastBlock }
