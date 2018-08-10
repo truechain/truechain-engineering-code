cf65de0142b1cfac15791ee96a91a479b575f934
Ashton <cdljsj@163.com>
2018-08-10 11:23:26 +0800

    add db schema prefix for snailchain

--- core/snailchain/rawdb/schema.go
+++ core/snailchain/rawdb/schema.go
@@ -42,23 +42,23 @@ var (
 	fastTrieProgressKey = []byte("TrieSync")
 
 	// Data item prefixes (use single byte to avoid mixing data types, avoid `i`, used for indexes).
-	headerPrefix          = []byte("h") // headerPrefix + num (uint64 big endian) + hash -> header
-	headerTDSuffix        = []byte("t") // headerPrefix + num (uint64 big endian) + hash + headerTDSuffix -> td
-	headerHashSuffix      = []byte("n") // headerPrefix + num (uint64 big endian) + headerHashSuffix -> hash
-	headerNumberPrefix    = []byte("H") // headerNumberPrefix + hash -> num (uint64 big endian)
-	headerCommitteeSuffix = []byte("C") // headerPrefix + num (uint64 big endian) + hash + headerCommitteeSuffix -> committee
+	headerPrefix          = []byte("sh") // headerPrefix + num (uint64 big endian) + hash -> header
+	headerTDSuffix        = []byte("st") // headerPrefix + num (uint64 big endian) + hash + headerTDSuffix -> td
+	headerHashSuffix      = []byte("sn") // headerPrefix + num (uint64 big endian) + headerHashSuffix -> hash
+	headerNumberPrefix    = []byte("sH") // headerNumberPrefix + hash -> num (uint64 big endian)
+	headerCommitteeSuffix = []byte("sC") // headerPrefix + num (uint64 big endian) + hash + headerCommitteeSuffix -> committee
 
-	blockBodyPrefix     = []byte("b") // blockBodyPrefix + num (uint64 big endian) + hash -> block body
-	blockReceiptsPrefix = []byte("r") // blockReceiptsPrefix + num (uint64 big endian) + hash -> block receipts
+	blockBodyPrefix     = []byte("sb") // blockBodyPrefix + num (uint64 big endian) + hash -> block body
+	blockReceiptsPrefix = []byte("sr") // blockReceiptsPrefix + num (uint64 big endian) + hash -> block receipts
 
-	txLookupPrefix  = []byte("l") // txLookupPrefix + hash -> transaction/receipt lookup metadata
-	bloomBitsPrefix = []byte("B") // bloomBitsPrefix + bit (uint16 big endian) + section (uint64 big endian) + hash -> bloom bits
+	txLookupPrefix  = []byte("sl") // txLookupPrefix + hash -> transaction/receipt lookup metadata
+	bloomBitsPrefix = []byte("sB") // bloomBitsPrefix + bit (uint16 big endian) + section (uint64 big endian) + hash -> bloom bits
 
-	preimagePrefix = []byte("secure-key-")      // preimagePrefix + hash -> preimage
-	configPrefix   = []byte("ethereum-config-") // config prefix for the db
+	preimagePrefix = []byte("snailchain-secure-key-")      // preimagePrefix + hash -> preimage
+	configPrefix   = []byte("snailchain-ethereum-config-") // config prefix for the db
 
 	// Chain index prefixes (use `i` + single byte to avoid mixing data types).
-	BloomBitsIndexPrefix = []byte("iB") // BloomBitsIndexPrefix is the data table of a chain indexer to track its progress
+	BloomBitsIndexPrefix = []byte("siB") // BloomBitsIndexPrefix is the data table of a chain indexer to track its progress
 
 	preimageCounter    = metrics.NewRegisteredCounter("db/preimage/total", nil)
 	preimageHitCounter = metrics.NewRegisteredCounter("db/preimage/hits", nil)
