3b2d82f3df281711660867c50ca96dd79677a70c
shijinyuan <13152720003@163.com>
2018-08-13 17:27:03 +0800

    core/fastchain:Change directory structure

--- core/events.go
+++ core/events.go
@@ -19,10 +19,14 @@ package core
 import (
 	"github.com/truechain/truechain-engineering-code/common"
 	"github.com/truechain/truechain-engineering-code/core/types"
+	"math/big"
 )
 
 // NewTxsEvent is posted when a batch of transactions enter the transaction pool.
 type NewTxsEvent struct{ Txs []*types.Transaction }
+
+//for fruit and record
+type NewFruitsEvent struct{ Fruits []*types.SnailBlock }
 
 // PendingLogsEvent is posted pre mining and notifies of pending logs.
 type PendingLogsEvent struct {
@@ -32,8 +36,13 @@ type PendingLogsEvent struct {
 // PendingStateEvent is posted pre mining and notifies of pending state changes.
 type PendingStateEvent struct{}
 
-// NewMinedBlockEvent is posted when a block has been imported.
-type NewMinedBlockEvent struct{ Block *types.Block }
+type NewMinedSnailBlockEvent struct{ Block *types.SnailBlock }
+
+
+
+// NewMinedFruitEvent is posted when a block has been imported.
+type NewMinedFruitEvent struct{ Block *types.SnailBlock }
+
 
 // RemovedLogsEvent is posted when a reorg happens
 type RemovedLogsEvent struct{ Logs []*types.Log }
@@ -45,9 +54,31 @@ type ChainEvent struct {
 	Logs  []*types.Log
 }
 
-
 type ChainSideEvent struct {
 	Block *types.Block
 }
 
 type ChainHeadEvent struct{ Block *types.Block }
+
+// for fruit event
+type FruitEvent struct {
+	Block *types.Block
+	Hash  common.Hash
+	Logs  []*types.Log
+}
+
+
+type ElectionEvent struct {
+	Option 	uint
+	Fastnumber	*big.Int
+	Snailnumber *big.Int
+}
+
+type CommitteeEvent struct {
+	CommitteeInfo *types.CommitteeInfo
+}
+
+type PbftSignEvent struct{ PbftSigns []*types.PbftSign }
+
+// NewFastBlockEvent is posted when a block has been generate .
+type NewFastBlockEvent struct{ FastBlock *types.Block}
