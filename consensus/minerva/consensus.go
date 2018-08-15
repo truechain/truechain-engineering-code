aedc9bdb0de9a6378cc604aef68926368cc91d90
wbqlove <wbqlove1234@163.com>
2018-08-15 15:21:37 +0800

    consensus change interface par

--- consensus/minerva/consensus.go
+++ consensus/minerva/consensus.go
@@ -780,7 +780,7 @@ func (m *Minerva) FinalizeFast(chain consensus.ChainFastReader, header *types.He
 //gas allocation
 func (m *Minerva) FinalizeFastGas(state *state.StateDB, fastNumber *big.Int, fastHash common.Hash, gasLimit *big.Int) error {
 	var ce consensus.CommitteeElection
-	_, committee := ce.GetCommittee(fastNumber, fastHash)
+	committee := ce.GetCommittee(fastNumber)
 	committeeGas := new(big.Int).Div(gasLimit, big.NewInt(int64(len(committee))))
 	for _, v := range committee {
 		state.AddBalance(v.Coinbase, committeeGas)
