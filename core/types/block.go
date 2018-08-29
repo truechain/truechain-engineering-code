2d80891b5597f51e689646b9ec71a65791bad0ec
tianming <tianming@shuangchuangpuhui.com>
2018-08-29 15:36:14 +0800

    core/types/block.go: update eth to etrue

--- core/types/block.go
+++ core/types/block.go
@@ -207,7 +207,7 @@ type Block struct {
 	// of the chain up to and including the block.
 	// td *big.Int
 
-	// These fields are used by package eth to track
+	// These fields are used by package etrue to track
 	// inter-peer block relay.
 	ReceivedAt   time.Time
 	ReceivedFrom interface{}
@@ -291,7 +291,7 @@ func CopyHeader(h *Header) *Header {
 	return &cpy
 }
 
-// "external" block encoding. used for eth protocol, etc.
+// "external" block encoding. used for etrue protocol, etc.
 type extblock struct {
 	Header *Header
 	Txs    []*Transaction
@@ -488,13 +488,13 @@ type SnailBlock struct {
 	// of the chain up to and including the block.
 	td *big.Int
 
-	// These fields are used by package eth to track
+	// These fields are used by package etrue to track
 	// inter-peer block relay.
 	ReceivedAt   time.Time
 	ReceivedFrom interface{}
 }
 
-// "external" block encoding. used for eth protocol, etc.
+// "external" block encoding. used for etrue protocol, etc.
 type extsnailblock struct {
 	Header *SnailHeader
 	Fruits []*SnailBlock
