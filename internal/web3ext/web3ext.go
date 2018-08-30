ea23636600bc44126c86509c87f18753f56b5d88
tianming <tianming@shuangchuangpuhui.com>
2018-08-30 13:26:35 +0800

    web3ext.go;rpc/client.go: rename etrue for api prefix

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -396,37 +396,37 @@ web3._extend({
 	methods: [
 		new web3._extend.Method({
 			name: 'sign',
-			call: 'true_sign',
+			call: 'etrue_sign',
 			params: 2,
 			inputFormatter: [web3._extend.formatters.inputAddressFormatter, null]
 		}),
 		new web3._extend.Method({
 			name: 'resend',
-			call: 'true_resend',
+			call: 'etrue_resend',
 			params: 3,
 			inputFormatter: [web3._extend.formatters.inputTransactionFormatter, web3._extend.utils.fromDecimal, web3._extend.utils.fromDecimal]
 		}),
 		new web3._extend.Method({
 			name: 'signTransaction',
-			call: 'true_signTransaction',
+			call: 'etrue_signTransaction',
 			params: 1,
 			inputFormatter: [web3._extend.formatters.inputTransactionFormatter]
 		}),
 		new web3._extend.Method({
 			name: 'submitTransaction',
-			call: 'true_submitTransaction',
+			call: 'etrue_submitTransaction',
 			params: 1,
 			inputFormatter: [web3._extend.formatters.inputTransactionFormatter]
 		}),
 		new web3._extend.Method({
 			name: 'getRawTransaction',
-			call: 'true_getRawTransactionByHash',
+			call: 'etrue_getRawTransactionByHash',
 			params: 1
 		}),
 		new web3._extend.Method({
 			name: 'getRawTransactionFromBlock',
 			call: function(args) {
-				return (web3._extend.utils.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'true_getRawTransactionByBlockHashAndIndex' : 'true_getRawTransactionByBlockNumberAndIndex';
+				return (web3._extend.utils.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'etrue_getRawTransactionByBlockHashAndIndex' : 'etrue_getRawTransactionByBlockNumberAndIndex';
 			},
 			params: 2,
 			inputFormatter: [web3._extend.formatters.inputBlockNumberFormatter, web3._extend.utils.toHex]
@@ -435,7 +435,7 @@ web3._extend({
 	properties: [
 		new web3._extend.Property({
 			name: 'pendingTransactions',
-			getter: 'true_pendingTransactions',
+			getter: 'etrue_pendingTransactions',
 			outputFormatter: function(txs) {
 				var formatted = [];
 				for (var i = 0; i < txs.length; i++) {
