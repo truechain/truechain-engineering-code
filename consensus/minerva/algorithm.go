77770fc2660cf77af679ba3c1784a6b6d32a8269
LIUYang <hyangl@163.com>
2018-09-04 11:37:40 +0800

    consensus/minerva: fix build error(#19).

--- consensus/minerva/algorithm.go
+++ consensus/minerva/algorithm.go
@@ -27,11 +27,11 @@ import (
 	"time"
 	"unsafe"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/bitutil"
+	"github.com/truechain/truechain-engineering-code/common"
+	"github.com/truechain/truechain-engineering-code/common/bitutil"
 	//	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/truechain/truechain-engineering-code/crypto/sha3"
+	"github.com/truechain/truechain-engineering-code/log"
 )
 
 const (
