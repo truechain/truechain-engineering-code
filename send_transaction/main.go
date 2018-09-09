5b95b4d1c6d6905f96acf68275e806761f3c730a
wbqlove <wbqlove1234@163.com>
2018-09-09 16:34:52 +0800

    consensus add log add send_transaction

--- send_transaction/main.go
+++ send_transaction/main.go
@@ -10,21 +10,34 @@ import (
 
 var Count int64
 
+var from, to int
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("invalid args : %s count [\"ip:port\"]\n", os.Args[0])
+	if len(os.Args) < 4 {
+		fmt.Printf("invalid args : %s [count] [from] [to]  [\"ip:port\"]\n", os.Args[0])
 		return
 	}
-
+	from = 0
+	to = 1
 	count, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		fmt.Println("count err")
 		return
 	}
 
-	ip := "127.0.0.1:8899"
+	from, err = strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("from err default 0")
+	}
+
+	to, err = strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Println("from err default 1")
+	}
+
+	ip := "127.0.0.1:8888"
 	if len(os.Args) == 3 {
-		ip = os.Args[2]
+		ip = os.Args[4]
 	}
 
 	send(count, ip)
@@ -85,8 +98,8 @@ func send(count int, ip string) {
 func sendTransaction(client *rpc.Client, account []string, wait *sync.WaitGroup) {
 	defer wait.Done()
 	map_data := make(map[string]string)
-	map_data["from"] = account[0]
-	map_data["to"] = account[1]
+	map_data["from"] = account[from]
+	map_data["to"] = account[to]
 	map_data["value"] = "0x2100"
 	var result string
 	client.Call(&result, "etrue_sendTransaction", map_data)
