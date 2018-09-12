f125eb8ba32fec79ade7ed070a53721e9a05d63a
shijinyuan <13152720003@163.com>
2018-09-12 20:22:45 +0800

    Merge the latest code

--- core/blockchain.go
+++ core/blockchain.go
@@ -262,8 +262,8 @@ func (bc *BlockChain) loadLastState() error {
 
 	rewardHead := rawdb.ReadHeadRewardNumber(bc.db)
 	if rewardHead != 0 {
-		rawdb.ReadBlockReward(bc.db, rewardHead)
-		reward := bc.GetFastHeightBySnailHeight(rewardHead)
+		reward := rawdb.ReadBlockReward(bc.db, rewardHead)
+		//reward := bc.GetFastHeightBySnailHeight(rewardHead)
 		bc.currentReward.Store(reward)
 	}
 
@@ -960,6 +960,8 @@ func (bc *BlockChain) WriteBlockWithState(block *types.Block, receipts []*types.
 		//insert BlockReward to db
 		rawdb.WriteBlockReward(batch, br)
 		rawdb.WriteHeadRewardNumber(bc.db, block.SnailNumber().Uint64())
+
+		bc.currentReward.Store(br)
 	}
 
 	root, err := state.Commit(bc.chainConfig.IsEIP158(block.Number()))
