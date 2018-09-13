028e63f8c1321c3c1509c88a0c158220ffbf73d0
tianming <tianming@shuangchuangpuhui.com>
2018-09-13 16:03:55 +0800

    ed25519.go: fixed bugs

--- vendor/github.com/ed25519/ed25519.go
+++ vendor/github.com/ed25519/ed25519.go
@@ -26,11 +26,18 @@ const (
 // GenerateKey generates a public/private key pair using randomness from rand.
 func GenerateKey(rand io.Reader) (publicKey *[PublicKeySize]byte, privateKey *[PrivateKeySize]byte, err error) {
 	privateKey = new([64]byte)
-	publicKey = new([32]byte)
 	_, err = io.ReadFull(rand, privateKey[:32])
 	if err != nil {
 		return nil, nil, err
 	}
+
+	publicKey = MakePublicKey(privateKey)
+	return
+}
+
+// MakePublicKey makes a publicKey from the first half of privateKey.
+func MakePublicKey(privateKey *[PrivateKeySize]byte) (publicKey *[PublicKeySize]byte) {
+	publicKey = new([32]byte)
 
 	h := sha512.New()
 	h.Write(privateKey[:32])
