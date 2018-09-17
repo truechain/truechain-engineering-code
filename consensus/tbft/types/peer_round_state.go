ebb4929bb02543b1a53f955c5f752990d4e0bc2a
wbqlove <wbqlove1234@163.com>
2018-09-17 20:59:34 +0800

    update tbft start

--- consensus/tbft/types/peer_round_state.go
+++ consensus/tbft/types/peer_round_state.go
@@ -2,8 +2,8 @@ package types
 
 import (
 	"fmt"
-	"time"
 	"github.com/truechain/truechain-engineering-code/consensus/tbft/help"
+	"time"
 )
 
 //-----------------------------------------------------------------------------
@@ -11,21 +11,21 @@ import (
 // PeerRoundState contains the known state of a peer.
 // NOTE: Read-only when returned by PeerState.GetRoundState().
 type PeerRoundState struct {
-	Height                   int64               `json:"height"`                      // Height peer is at
-	Round                    int                 `json:"round"`                       // Round peer is at, -1 if unknown.
-	Step                     RoundStepType       `json:"step"`                        // Step peer is at
-	StartTime                time.Time           `json:"start_time"`                  // Estimated start of round 0 at this height
-	Proposal                 bool                `json:"proposal"`                    // True if peer has proposal for this round
-	ProposalBlockPartsHeader PartSetHeader 		 `json:"proposal_block_parts_header"` //
-	ProposalBlockParts       *help.BitArray      `json:"proposal_block_parts"`        //
-	ProposalPOLRound         int                 `json:"proposal_pol_round"`          // Proposal's POL round. -1 if none.
-	ProposalPOL              *help.BitArray      `json:"proposal_pol"`                // nil until ProposalPOLMessage received.
-	Prevotes                 *help.BitArray      `json:"prevotes"`                    // All votes peer has for this round
-	Precommits               *help.BitArray      `json:"precommits"`                  // All precommits peer has for this round
-	LastCommitRound          int                 `json:"last_commit_round"`           // Round of commit for last height. -1 if none.
-	LastCommit               *help.BitArray      `json:"last_commit"`                 // All commit precommits of commit for last height.
-	CatchupCommitRound       int                 `json:"catchup_commit_round"`        // Round that we have commit for. Not necessarily unique. -1 if none.
-	CatchupCommit            *help.BitArray      `json:"catchup_commit"`              // All commit precommits peer has for this height & CatchupCommitRound
+	Height                   uint64         `json:"height"`                      // Height peer is at
+	Round                    uint           `json:"round"`                       // Round peer is at, -1 if unknown.
+	Step                     RoundStepType  `json:"step"`                        // Step peer is at
+	StartTime                time.Time      `json:"start_time"`                  // Estimated start of round 0 at this height
+	Proposal                 bool           `json:"proposal"`                    // True if peer has proposal for this round
+	ProposalBlockPartsHeader PartSetHeader  `json:"proposal_block_parts_header"` //
+	ProposalBlockParts       *help.BitArray `json:"proposal_block_parts"`        //
+	ProposalPOLRound         uint           `json:"proposal_pol_round"`          // Proposal's POL round. -1 if none.
+	ProposalPOL              *help.BitArray `json:"proposal_pol"`                // nil until ProposalPOLMessage received.
+	Prevotes                 *help.BitArray `json:"prevotes"`                    // All votes peer has for this round
+	Precommits               *help.BitArray `json:"precommits"`                  // All precommits peer has for this round
+	LastCommitRound          uint           `json:"last_commit_round"`           // Round of commit for last height. -1 if none.
+	LastCommit               *help.BitArray `json:"last_commit"`                 // All commit precommits of commit for last height.
+	CatchupCommitRound       uint           `json:"catchup_commit_round"`        // Round that we have commit for. Not necessarily unique. -1 if none.
+	CatchupCommit            *help.BitArray `json:"catchup_commit"`              // All commit precommits peer has for this height & CatchupCommitRound
 }
 
 // String returns a string representation of the PeerRoundState
