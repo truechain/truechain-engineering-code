57f540453e0e07b751d366e4f29edc351b82e733
shijinyuan <13152720003@163.com>
2018-09-18 15:07:23 +0800

    downloader test

--- etrue/handler.go
+++ etrue/handler.go
@@ -1034,22 +1034,22 @@ func (pm *ProtocolManager) handleMsg(p *peer) error {
 		}
 
 		// downloads
-		var (
-			trueHead = request.Block.ParentHash()
-			trueTD   = new(big.Int).Sub(request.td, request.Block.Difficulty())
-		)
+		//var (
+		//	trueHead = request.Block.ParentHash()
+		//	trueTD   = new(big.Int).Sub(request.TD, request.Block.Difficulty())
+		//)
 		// Update the peers total difficulty if better than the previous
-		if _, td := p.Head(); trueTD.Cmp(td) > 0 {
-			p.SetHead(trueHead, trueTD)
-
-			// Schedule a sync if above ours. Note, this will not fire a sync for a gap of
-			// a singe block (as the true TD is below the propagated block), however this
-			// scenario should easily be covered by the fetcher.
-			currentBlock := pm.blockchain.CurrentBlock()
-			if trueTD.Cmp(pm.blockchain.GetTd(currentBlock.Hash(), currentBlock.NumberU64())) > 0 {
-				go pm.synchronise(p)
-			}
-		}
+		//if _, td := p.Head(); trueTD.Cmp(td) > 0 {
+		//	p.SetHead(trueHead, trueTD)
+		//
+		//	// Schedule a sync if above ours. Note, this will not fire a sync for a gap of
+		//	// a singe block (as the true TD is below the propagated block), however this
+		//	// scenario should easily be covered by the fetcher.
+		//	currentBlock := pm.blockchain.CurrentBlock()
+		//	if trueTD.Cmp(pm.blockchain.GetTd(currentBlock.Hash(), currentBlock.NumberU64())) > 0 {
+		//		go pm.synchronise(p)
+		//	}
+		//}
 
 	default:
 		return errResp(ErrInvalidMsgCode, "%v", msg.Code)
