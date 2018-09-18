a9c80b793d658baa4cf1ffde98cac2e485b6d248
wbqlove <wbqlove1234@163.com>
2018-09-18 13:05:28 +0800

    update tbft int(64) to uint(64)

--- consensus/tbft/types/event_data.go
+++ consensus/tbft/types/event_data.go
@@ -25,8 +25,8 @@ const (
 
 // NOTE: This goes into the replay WAL
 type EventDataRoundState struct {
-	Height int64  				`json:"height"`
-	Round  int    				`json:"round"`
+	Height uint64  				`json:"height"`
+	Round  uint    				`json:"round"`
 	Step   string 				`json:"step"`
 
 	// private, not exposed to websockets
