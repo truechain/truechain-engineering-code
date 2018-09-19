b1f2a3398ca90f711f17e770550336216e0b38b1
wbqlove <wbqlove1234@163.com>
2018-09-19 17:36:44 +0800

    tbft fix a bug

--- consensus/tbft/crypto/truekey.go
+++ consensus/tbft/crypto/truekey.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"crypto/ecdsa"
 	"fmt"
-	tcrypyo "github.com/truechain/truechain-engineering-code/crypto"
 	amino "github.com/truechain/truechain-engineering-code/consensus/tbft/go-amino"
 	"github.com/truechain/truechain-engineering-code/consensus/tbft/help"
+	tcrypyo "github.com/truechain/truechain-engineering-code/crypto"
 )
 
 //-------------------------------------
@@ -17,6 +17,7 @@ const (
 	// Edwards25519 point, and a field element. Both of which are 32 bytes.
 	SignatureEd25519Size = 64
 )
+
 var cdc = amino.NewCodec()
 
 func init() {
@@ -28,6 +29,7 @@ func init() {
 	cdc.RegisterConcrete(PrivKeyTrue{},
 		EcdsaPrivKeyAminoRoute, nil)
 }
+
 // PrivKeyTrue implements PrivKey.
 type PrivKeyTrue ecdsa.PrivateKey
 
@@ -39,17 +41,17 @@ func (priv PrivKeyTrue) Bytes() []byte {
 // Sign produces a signature on the provided message.
 func (priv PrivKeyTrue) Sign(msg []byte) ([]byte, error) {
 	priv1 := ecdsa.PrivateKey(priv)
-	return tcrypyo.Sign(msg,&priv1)
+	return tcrypyo.Sign(msg, &priv1)
 }
 
 // PubKey gets the corresponding public key from the private key.
 func (priv PrivKeyTrue) PubKey() PubKey {
 	priv1 := ecdsa.PrivateKey(priv)
-	pub0,ok := priv1.Public().(ecdsa.PublicKey)
+	pub0, ok := priv1.Public().(*ecdsa.PublicKey)
 	if !ok {
 		panic(0)
 	}
-	pub := PubKeyTrue(pub0)
+	pub := PubKeyTrue(*pub0)
 	return &pub
 }
 
@@ -70,7 +72,7 @@ func (priv PrivKeyTrue) Equals(other PrivKey) bool {
 // GenPrivKey generates a new ed25519 private key.
 
 func GenPrivKey() PrivKeyTrue {
-	priv,err := tcrypyo.GenerateKey()
+	priv, err := tcrypyo.GenerateKey()
 	if err != nil {
 		panic(err)
 	}
@@ -101,7 +103,7 @@ func (pub PubKeyTrue) Bytes() []byte {
 
 func (pub PubKeyTrue) VerifyBytes(msg []byte, sig_ []byte) bool {
 	// make sure we use the same algorithm to sign
-	if pub0,err := tcrypyo.SigToPub(msg,sig_); err == nil {
+	if pub0, err := tcrypyo.SigToPub(msg, sig_); err == nil {
 		pub1 := PubKeyTrue(*pub0)
 		return pub.Equals(pub1)
 	}
