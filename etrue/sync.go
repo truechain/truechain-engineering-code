ce70ffb41a5a565b6a07432b7b85318ceeb4a71f
shijinyuan <13152720003@163.com>
2018-09-22 14:59:00 +0800

    add Broadcast

--- etrue/sync.go
+++ etrue/sync.go
@@ -311,4 +311,13 @@ func (pm *ProtocolManager) synchronise(peer *peer) {
 		// more reliably update peers or the local TD state.
 		go pm.BroadcastSnailBlock(head, false)
 	}
+	if head := pm.blockchain.CurrentBlock(); head.NumberU64() > 0 {
+		// We've completed a sync cycle, notify all peers of new state. This path is
+		// essential in star-topology networks where a gateway node needs to notify
+		// all its out-of-date peers of the availability of a new block. This failure
+		// scenario will most often crop up in private and hackathon networks with
+		// degenerate connectivity, but it should be healthy for the mainnet too to
+		// more reliably update peers or the local TD state.
+		go pm.BroadcastFastBlock(head, false)
+	}
 }
