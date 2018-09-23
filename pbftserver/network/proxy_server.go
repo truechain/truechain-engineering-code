775cad17d9f56b35711165694be5c27c6988923e
wbqlove <wbqlove1234@163.com>
2018-09-24 00:17:52 +0800

    pbftserver stop for message

--- pbftserver/network/proxy_server.go
+++ pbftserver/network/proxy_server.go
@@ -68,7 +68,9 @@ func NewServer(nodeID string, id *big.Int, help consensus.ConsensusHelp,
 	server := &Server{ID: new(big.Int).Set(id), help: help}
 	node := NewNode(nodeID, verify, server, addrs, id)
 	server.Node = node
+	server.Node.NTLock.Lock()
 	server.url = node.NodeTable[nodeID]
+	server.Node.NTLock.Unlock()
 	server.server = &http.Server{
 		Addr: server.url,
 	}
