117a1e8cfac8beafa60ca44060c882f16b199d50
shuxun <shuxunyer@163.com>
2018-09-26 07:41:29 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	etrue/pbft_agent.go  add initNodework method

--- etrue/pbft_agent.go
+++ etrue/pbft_agent.go
@@ -161,20 +161,25 @@ func NewPbftAgent(eth Backend, config *params.ChainConfig, engine consensus.Engi
 		cacheBlockMu:         new(sync.Mutex),
 		cacheBlock:           make(map[*big.Int]*types.Block),
 	}
-	nodeInfoWork := &nodeInfoWork{
-		cacheSign:     make(map[string]types.Sign),
-		ticker:        time.NewTicker(sendNodeTime),
-		committeeInfo: new(types.CommitteeInfo),
-	}
-	self.nodeInfoWork1 = nodeInfoWork
-	self.nodeInfoWork2 = nodeInfoWork
-
+	self.initNodeWork()
 	self.InitNodeInfo(eth.Config())
 	if !self.singleNode {
 		self.electionSub = self.election.SubscribeElectionEvent(self.electionCh)
 		self.chainHeadAgentSub = self.fastChain.SubscribeChainHeadEvent(self.chainHeadCh)
 	}
 	return self
+}
+
+func (self *PbftAgent) initNodeWork() {
+	nodeInfoWork := &nodeInfoWork{
+		cacheSign:     make(map[string]types.Sign),
+		ticker:        time.NewTicker(sendNodeTime),
+		committeeInfo: new(types.CommitteeInfo),
+	}
+	self.nodeInfoWork1 = nodeInfoWork
+	self.nodeInfoWork1.tag = 1
+	self.nodeInfoWork2 = nodeInfoWork
+	self.nodeInfoWork2.tag = 2
 }
 
 func (self *PbftAgent) InitNodeInfo(config *Config) {
@@ -211,6 +216,7 @@ func (self *PbftAgent) stop() {
 }
 
 type nodeInfoWork struct {
+	tag               int
 	committeeInfo     *types.CommitteeInfo
 	cryptoNode        *types.EncryptNodeMessage
 	cacheSign         map[string]types.Sign //prevent receive same sign
@@ -223,12 +229,10 @@ func (self *PbftAgent) getStartNodeWork() *nodeInfoWork {
 	if self.nodeInfoWork1.isCurrent {
 		self.nodeInfoWork1.isCurrent = false
 		self.nodeInfoWork2.isCurrent = true
-		log.Info("get nodeInfoWork2")
 		return self.nodeInfoWork2
 	} else {
 		self.nodeInfoWork1.isCurrent = true
 		self.nodeInfoWork2.isCurrent = false
-		log.Info("get nodeInfoWork1")
 		return self.nodeInfoWork1
 	}
 }
@@ -257,12 +261,12 @@ func (self *PbftAgent) stopSend() {
 }
 func (self *PbftAgent) debugNodeInfoWork(node *nodeInfoWork, str string) {
 	if node.cryptoNode == nil {
-		log.Info(str, "isMember", node.isCommitteeMember, "isCurrent", node.isCurrent,
+		log.Info(str, "tag", node.tag, "isMember", node.isCommitteeMember, "isCurrent", node.isCurrent,
 			"nodeWork1", self.nodeInfoWork1.isCurrent, "nodeWork2", self.nodeInfoWork2.isCurrent,
 			"committeeId", node.committeeInfo.Id, "committeeInfoMembers", len(node.committeeInfo.Members),
 			"cacheSignLen", len(node.cacheSign), "len(cryptoNode.Nodes)", "nil")
 	} else {
-		log.Info(str, "isMember", node.isCommitteeMember, "isCurrent", node.isCurrent,
+		log.Info(str, "tag", node.tag, "isMember", node.isCommitteeMember, "isCurrent", node.isCurrent,
 			"nodeWork1", self.nodeInfoWork1.isCurrent, "nodeWork2", self.nodeInfoWork2.isCurrent,
 			"committeeId", node.committeeInfo.Id, "committeeInfoMembers", len(node.committeeInfo.Members),
 			"cacheSignLen", len(node.cacheSign), "len(cryptoNode.Nodes)", len(node.cryptoNode.Nodes))
