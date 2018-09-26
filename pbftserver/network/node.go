30966f55562c83db70c23fa840360d6a44397fca
wbqlove <wbqlove1234@163.com>
2018-09-26 16:20:29 +0800

    pbftserver remove data

--- pbftserver/network/node.go
+++ pbftserver/network/node.go
@@ -141,7 +141,7 @@ func (node *Node) Broadcast(msg interface{}, path string) map[string]error {
 			errorMap[nodeID] = err
 			continue
 		}
-		lock.PSLog("Broadcast", url+path, string(jsonMsg))
+		lock.PSLog("Broadcast", url+path)
 		go send(url+path, jsonMsg)
 
 	}
