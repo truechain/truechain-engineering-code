a4cf6ae0f574dd6a37d2b7d7a2ef145a6d12dee7
LIUYang <hyangl@163.com>
2018-10-11 22:06:55 +0800

    core/snailchain: fix bug when validate snail block fruits.

--- core/snailchain/block_validator.go
+++ core/snailchain/block_validator.go
@@ -120,18 +120,14 @@ func (v *BlockValidator) ValidateBody(block *types.SnailBlock) error {
 	}
 	for _, fruit := range block.Fruits() {
 		if fruit.FastNumber().Uint64() - temp != 1 {
+			log.Info("ValidateBody snail validate fruit error", "fruit", fruit.FastNumber(), "block", block.Number(), "pre", temp)
 			return ErrInvalidFruits
 		}
-		temp = fruit.FastNumber().Uint64()
 		if err := v.ValidateFruit(fruit, block); err != nil {
 			log.Info("ValidateBody snail validate fruit error", "fruit", fruit.FastNumber(), "block", block.Number(), "err", err)
 			return err
 		}
-		// TODO: check fruit order
-		if fruit.FastNumber().Uint64() - temp != 1 {
-			log.Info("ValidateBody snail validate fruit error", "fruit", fruit.FastNumber(), "block", block.Number(), "pre", temp)
-			return ErrInvalidFruits
-		}
+
 		temp = fruit.FastNumber().Uint64()
 	}
 
