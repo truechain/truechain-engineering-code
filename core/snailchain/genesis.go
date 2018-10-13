45a87a811d1ec313dd744b539d8f19a2c83699f9
shuxun <shuxunyer@163.com>
2018-10-13 20:28:44 +0800

    genesis.go update difficult

--- core/snailchain/genesis.go
+++ core/snailchain/genesis.go
@@ -228,7 +228,7 @@ func DefaultGenesisBlock() *Genesis {
 		Nonce:      66,
 		ExtraData:  nil,
 		GasLimit:   88080384,
-		Difficulty: big.NewInt(600000),
+		Difficulty: big.NewInt(1000000),
 		//Alloc:      decodePrealloc(mainnetAllocData),
 		Alloc: map[common.Address]types.GenesisAccount{
 			common.HexToAddress("0x7c357530174275dd30e46319b89f71186256e4f7"): {Balance: i},
