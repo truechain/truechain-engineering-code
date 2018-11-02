9d8ebd2df075f7903fa9d693203138883e6a3a01
tm <ice_ming@126.com>
2018-11-02 11:19:53 +0800

    state.go: fixed bug for init lockedblock

--- consensus/tbft/state.go
+++ consensus/tbft/state.go
@@ -1028,8 +1028,10 @@ func (cs *ConsensusState) enterPrecommit(height uint64, round int) {
 	// At this point, +2/3 prevoted for a particular block.
 
 	// If we're already locked on that block, precommit it, and update the LockedRound
-	tmpBlock := cs.LockedBlock.Hash()
-	if help.EqualHashes(tmpBlock[:],blockID.Hash) {
+	if (cs.LockedBlock != nil) &&  func() bool {
+			hash := cs.LockedBlock.Hash()
+			return help.EqualHashes(hash[:],blockID.Hash)
+	}() {
 		log.Info("enterPrecommit: +2/3 prevoted locked block. Relocking")
 		cs.LockedRound = uint(round)
 		cs.eventBus.PublishEventRelock(cs.RoundStateEvent())
