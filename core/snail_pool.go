65d5ae08fe1c7fb3c20a0d3e492d7513a6137520
ywzqwwt <752252086@qq.com>
2018-11-09 16:06:10 +0800

    remove unuse attribute

--- core/snail_pool.go
+++ core/snail_pool.go
@@ -111,17 +111,6 @@ type SnailPoolConfig struct {
 	NoLocals  bool          // Whether local transaction handling should be disabled
 	Journal   string        // Journal of local fruits to survive node restarts
 	Rejournal time.Duration // Time interval to regenerate the local fruit journal
-
-	PriceLimit uint64 // Minimum gas price to enforce for acceptance into the pool
-	PriceBump  uint64 // Minimum price bump percentage to replace an already existing transaction (nonce)
-
-	AccountSlots uint64 // Minimum number of executable transaction slots guaranteed per account
-	GlobalSlots  uint64 // Maximum number of executable transaction slots for all accounts
-	AccountQueue uint64 // Maximum number of non-executable transaction slots permitted per account
-	GlobalQueue  uint64 // Maximum number of non-executable transaction slots for all accounts
-
-	Lifetime time.Duration // Maximum amount of time non-executable transaction are queued
-
 	FruitCount uint64
 	FastCount  uint64
 }
@@ -131,18 +120,7 @@ type SnailPoolConfig struct {
 var DefaultSnailPoolConfig = SnailPoolConfig{
 	//Journal: "fruits.rlp",
 	Journal: "fruits.rlp",
-	//Journal:   "fastBlocks.rlp",
 	Rejournal: time.Hour,
-
-	PriceLimit: 1,
-	PriceBump:  10,
-
-	AccountSlots: 16,
-	GlobalSlots:  4096,
-	AccountQueue: 64,
-	GlobalQueue:  1024,
-
-	Lifetime:   3 * time.Hour,
 	FruitCount: 8192,
 	FastCount:  8192,
 }
