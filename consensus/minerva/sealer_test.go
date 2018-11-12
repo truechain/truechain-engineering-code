7edee6437275e366f40679a9e5b6cb7973bd5e84
BiyunZeng <chhaozeng@163.com>
2018-11-12 14:14:09 +0800

    consensus/minerva/sealer.go: fix sealer_test error

--- consensus/minerva/sealer_test.go
+++ consensus/minerva/sealer_test.go
@@ -4,22 +4,41 @@ import (
 	"math/rand"
 	"testing"
 	"time"
-)
+	"github.com/truechain/truechain-engineering-code/core/types"
 
+)
+type SnailChainReader interface {
+
+	// GetHeaderByNumber retrieves a block header from the database by number.
+	GetHeaderByNumber(number uint64) *types.SnailHeader
+
+}
 //TestUpdateLookup Feed datasets to update to get new matrix data
 func TestUpdateLookup(t *testing.T) {
-	//minerva := NewTester()
+	minerva := NewTester()
 	var b [32]byte
 	for i := 0; i < 32; i++ {
 		b[i] = RandUint() % 255
 	}
-	//dataset:= make([]uint64, TBLSIZE*DATALENGTH*PMTSIZE*32)
+
+	/*
+	for i:=1;i<1000000000;i++{
+		if (i%12000)>10240{
+			fmt.Println("i%12000 = %d,     i = %d",i%12000,i)
+		}
+
+	}*/
+
+
+	//minerva.SetSnailChainReader(SnailChainReader)
+	dataset:= make([]uint64, TBLSIZE*DATALENGTH*PMTSIZE*32)
 	//flag,dataset := minerva.updateLookupTBL(22250,dataset)
-	//if flag {
-	//	t.Log("dataset:",dataset)
-	//}else {
-	//	t.Error("======update-err=====",flag)
-	//}
+	flag,dataset := minerva.updateLookupTBL(191788,dataset)
+	if flag {
+		t.Log("dataset:",dataset)
+	}else {
+		t.Error("======update-err=====",flag)
+	}
 }
 
 func RandUint() uint8 {
