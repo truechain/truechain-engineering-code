d4f43c3d17a1caada8cd38e832f071713a45ea27
jiyilanzhou <450595468@qq.com>
2018-11-13 18:10:24 +0800

    etrue/fetcher/fetcher: add fetcher drop peer

--- etrue/fetcher/fetcher.go
+++ etrue/fetcher/fetcher.go
@@ -627,7 +627,7 @@ func (f *Fetcher) loop() {
 				if announce := f.fetching[hash]; announce != nil && announce.origin == task.peer && f.fetched[hash] == nil && f.completing[hash] == nil && f.getPendingBlock(hash) == nil {
 					// If the delivered header does not match the promised number, drop the announcer
 					if header.Number.Uint64() != announce.number {
-						log.Trace("Invalid fast block number fetched", "peer", announce.origin, "hash", header.Hash(), "announced", announce.number, "provided", header.Number)
+						log.Info("Invalid fast block number fetched", "peer", announce.origin, "hash", header.Hash(), "announced", announce.number, "provided", header.Number)
 						f.dropPeer(announce.origin)
 						f.forgetHash(hash)
 						continue
