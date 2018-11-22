59766404cce939cf4948082f68949afa3dc37906
wbqlove <wbqlove1234@163.com>
2018-11-22 11:47:17 +0800

    consensus/tbft/tbft_test.go add committee change test

--- consensus/tbft/tbft_test.go
+++ consensus/tbft/tbft_test.go
@@ -362,6 +362,152 @@ func TestPbftRunFor4(t *testing.T) {
 	<-start
 }
 
+func TestPbftRunFor4AndChange(t *testing.T) {
+	//log.OpenLogDebug(4)
+	IdCacheInit()
+	start := make(chan int)
+	pr1 := getPrivateKey(0)
+	pr2 := getPrivateKey(1)
+	pr3 := getPrivateKey(2)
+	pr4 := getPrivateKey(3)
+
+	agent1 := NewPbftAgent("Agent1")
+	agent2 := NewPbftAgent("Agent2")
+	agent3 := NewPbftAgent("Agent3")
+	agent4 := NewPbftAgent("Agent4")
+
+	config1 := new(config.Config)
+	*config1 = *config.TestConfig()
+	p2p1 := new(config.P2PConfig)
+	*p2p1 = *config1.P2P
+	p2p1.ListenAddress = "tcp://127.0.0.1:28890"
+	p2p1.ExternalAddress = "tcp://127.0.0.1:28891"
+	*config1.P2P = *p2p1
+
+	con1 := new(config.ConsensusConfig)
+	*con1 = *config1.Consensus
+	con1.WalPath = filepath.Join("data", "cs.wal1", "wal")
+	*config1.Consensus = *con1
+
+	n1, _ := NewNode(config1, "1", pr1, agent1)
+	n1.Start()
+
+	config2 := new(config.Config)
+	*config2 = *config.TestConfig()
+	p2p2 := new(config.P2PConfig)
+	*p2p2 = *config2.P2P
+	p2p2.ListenAddress = "tcp://127.0.0.1:28893"
+	p2p2.ExternalAddress = "tcp://127.0.0.1:28894"
+	*config2.P2P = *p2p2
+
+	con2 := new(config.ConsensusConfig)
+	*con2 = *config2.Consensus
+	con2.WalPath = filepath.Join("data", "cs.wal2", "wal")
+	*config2.Consensus = *con2
+
+	n2, _ := NewNode(config2, "1", pr2, agent2)
+	n2.Start()
+
+	config3 := new(config.Config)
+	*config3 = *config.TestConfig()
+	p2p3 := new(config.P2PConfig)
+	*p2p3 = *config3.P2P
+	p2p3.ListenAddress = "tcp://127.0.0.1:28895"
+	p2p3.ExternalAddress = "tcp://127.0.0.1:28896"
+	*config3.P2P = *p2p3
+
+	con3 := new(config.ConsensusConfig)
+	*con3 = *config3.Consensus
+	con3.WalPath = filepath.Join("data", "cs.wal3", "wal")
+	*config3.Consensus = *con3
+
+	n3, _ := NewNode(config3, "1", pr3, agent3)
+	n3.Start()
+
+	config4 := new(config.Config)
+	*config4 = *config.TestConfig()
+	p2p4 := new(config.P2PConfig)
+	*p2p4 = *config4.P2P
+	p2p4.ListenAddress = "tcp://127.0.0.1:28897"
+	p2p4.ExternalAddress = "tcp://127.0.0.1:28898"
+	*config4.P2P = *p2p4
+
+	con4 := new(config.ConsensusConfig)
+	*con4 = *config4.Consensus
+	con4.WalPath = filepath.Join("data", "cs.wal4", "wal")
+	*config4.Consensus = *con4
+
+	n4, _ := NewNode(config4, "1", pr4, agent4)
+	n4.Start()
+
+	c1 := new(types.CommitteeInfo)
+	c1.Id = big.NewInt(1)
+	m1 := new(types.CommitteeMember)
+	m1.Publickey = GetPub(pr1)
+	m1.Coinbase = common.Address{0}
+	m2 := new(types.CommitteeMember)
+	m2.Publickey = GetPub(pr2)
+	m2.Coinbase = common.Address{0}
+	m3 := new(types.CommitteeMember)
+	m3.Publickey = GetPub(pr3)
+	m3.Coinbase = common.Address{0}
+	m4 := new(types.CommitteeMember)
+	m4.Publickey = GetPub(pr4)
+	m4.Coinbase = common.Address{0}
+	c1.Members = append(c1.Members, m1, m2, m3, m4)
+	c1.StartHeight = common.Big0
+
+	cn := make([]*types.CommitteeNode, 0)
+	cn = append(cn, &types.CommitteeNode{IP: "127.0.0.1", Port: 28890, Coinbase: m1.Coinbase, Publickey: crypto.FromECDSAPub(m1.Publickey)})
+	cn = append(cn, &types.CommitteeNode{IP: "127.0.0.1", Port: 28893, Coinbase: m2.Coinbase, Publickey: crypto.FromECDSAPub(m2.Publickey)})
+	cn = append(cn, &types.CommitteeNode{IP: "127.0.0.1", Port: 28895, Coinbase: m3.Coinbase, Publickey: crypto.FromECDSAPub(m3.Publickey)})
+	cn = append(cn, &types.CommitteeNode{IP: "127.0.0.1", Port: 28897, Coinbase: m4.Coinbase, Publickey: crypto.FromECDSAPub(m4.Publickey)})
+
+	n1.PutCommittee(c1)
+	n1.PutNodes(common.Big1, cn)
+	n1.Notify(c1.Id, Start)
+
+	n2.PutCommittee(c1)
+	n2.PutNodes(common.Big1, cn)
+	n2.Notify(c1.Id, Start)
+
+	n3.PutCommittee(c1)
+	n3.PutNodes(common.Big1, cn)
+	n3.Notify(c1.Id, Start)
+
+	n4.PutCommittee(c1)
+	n4.PutNodes(common.Big1, cn)
+	n4.Notify(c1.Id, Start)
+
+	n1.SetCommitteeStop(c1.Id, 16)
+	n2.SetCommitteeStop(c1.Id, 16)
+	n3.SetCommitteeStop(c1.Id, 16)
+	n4.SetCommitteeStop(c1.Id, 16)
+
+	time.Sleep(100 * time.Second)
+
+	c2 := *c1
+	c2.Id = common.Big2
+
+	n1.PutCommittee(&c2)
+	n1.PutNodes(common.Big2, cn)
+	n1.Notify(c2.Id, Start)
+
+	n2.PutCommittee(&c2)
+	n2.PutNodes(common.Big2, cn)
+	n2.Notify(c2.Id, Start)
+
+	n3.PutCommittee(&c2)
+	n3.PutNodes(common.Big2, cn)
+	n3.Notify(c2.Id, Start)
+
+	n4.PutCommittee(&c2)
+	n4.PutNodes(common.Big2, cn)
+	n4.Notify(c2.Id, Start)
+
+	<-start
+}
+
 func TestPbftRunFor5(t *testing.T) {
 	log.OpenLogDebug(4)
 	IdCacheInit()
