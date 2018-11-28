633b9a04198e8f9d2ccc043bec31ec2ce1c9ae4e
wbqlove <wbqlove1234@163.com>
2018-11-28 15:57:42 +0800

    send_transaction/main.go Add a new feature when tx is full sleep

--- send_transaction/main.go
+++ send_transaction/main.go
@@ -17,7 +17,10 @@ var Count int64 = 0
 var from, to, frequency = 0, 1, 1
 
 //Two transmission intervals
-var interval = time.Millisecond * 0
+var interval  = time.Millisecond * 0
+
+//Send transmission full sleep intervals
+var sleep  = time.Millisecond * 10000
 
 //get all account
 var account []string
@@ -25,10 +28,16 @@ var account []string
 //time format
 var termTimeFormat = "01-02|15:04:05.000"
 
+//the pre count
+var preCount int64 = 0
+
+//flag sleep
+var  bSleep  = false
+
 // get par
 func main() {
 	if len(os.Args) < 4 {
-		fmt.Printf(getTime(), "invalid args : %s [count] [frequency] [interval] [from] [to]  [\"ip:port\"]\n", os.Args[0])
+		fmt.Printf( "invalid args : %s [count] [frequency] [interval] [sleep] [from] [to]  [\"ip:port\"]\n", os.Args[0])
 		return
 	}
 
@@ -52,19 +61,28 @@ func main() {
 		interval = time.Millisecond * time.Duration(intervalCount)
 	}
 
-	from, err = strconv.Atoi(os.Args[4])
+	sleepCnt, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println(getTime(), "sleep err default 10000")
+		return
+	} else {
+		sleep = time.Millisecond * time.Duration(sleepCnt)
+	}
+
+	from, err = strconv.Atoi(os.Args[5])
 	if err != nil {
 		fmt.Println(getTime(), "from err default 0")
 	}
 
-	to, err = strconv.Atoi(os.Args[5])
+	to, err = strconv.Atoi(os.Args[6])
 	if err != nil {
 		fmt.Println(getTime(), "from err default 1")
 	}
 
+
 	ip := "127.0.0.1:8888"
-	if len(os.Args) == 7 {
-		ip = os.Args[6]
+	if len(os.Args) == 8 {
+		ip = os.Args[7]
 	}
 
 	send(count, ip)
@@ -121,6 +139,10 @@ func send(count int, ip string) {
 	// send
 	waitMain := &sync.WaitGroup{}
 	for {
+		if bSleep{
+			bSleep = false
+			time.Sleep(sleep)
+		}
 		waitMain.Add(1)
 		go sendTransactions(client, account, count, waitMain)
 		frequency -= 1
@@ -132,7 +154,7 @@ func send(count int, ip string) {
 		err = client.Call(&result, "etrue_getBalance", account[from], "latest")
 		if err != nil {
 			fmt.Println(getTime(), "etrue_getBalance Error:", err)
-			return
+			//return
 		} else {
 			bl, _ := new(big.Int).SetString(result, 10)
 			fmt.Println(getTime(), "etrue_getBalance Ok:", bl, result)
@@ -154,6 +176,12 @@ func sendTransactions(client *rpc.Client, account []string, count int, wait *syn
 	fmt.Println(getTime(), "Send in go Complete", count)
 	waitGroup.Wait()
 	fmt.Println(getTime(), "Complete", Count)
+	if Count > preCount{
+		preCount = Count
+	}else {
+		fmt.Println(getTime(),"tx full sleep",sleep.Minutes())
+		bSleep = true
+	}
 }
 
 //send one transaction
