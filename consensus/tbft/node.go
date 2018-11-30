31f9416df763cd2cdc4ff1150263e346edab8cc5
wbqlove <wbqlove1234@163.com>
2018-11-30 15:00:57 +0800

    pbftserver remove log

--- consensus/tbft/node.go
+++ consensus/tbft/node.go
@@ -366,7 +366,6 @@ func (n *Node) PutCommittee(committeeInfo *types.CommitteeInfo) error {
 	return nil
 }
 func (n *Node) PutNodes(id *big.Int, nodes []*types.CommitteeNode) error {
-	log.Info("PutNodes", "info", nodes[0].String())
 	if id == nil || len(nodes) <= 0 {
 		return errors.New("wrong params...")
 	}
