39139c0d98f3f50f0ce6e291b8de97afe4ea206f
wbqlove <wbqlove1234@163.com>
2018-12-29 15:59:14 +0800

    tbft add switchInfos to blockBody

--- etrue/protocol.go
+++ etrue/protocol.go
@@ -22,9 +22,9 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/truechain/truechain-engineering-code/core/types"
 	"github.com/truechain/truechain-engineering-code/event"
-	"github.com/ethereum/go-ethereum/rlp"
 )
 
 // Constants to match up protocol versions and messages
@@ -79,7 +79,7 @@ const (
 type errCode int
 
 const (
-	ErrMsgTooLarge              = iota
+	ErrMsgTooLarge = iota
 	ErrDecode
 	ErrInvalidMsgCode
 	ErrProtocolVersionMismatch
@@ -228,6 +228,7 @@ type newSnailBlockData struct {
 type blockBody struct {
 	Transactions []*types.Transaction // Transactions contained within a block
 	Signs        []*types.PbftSign    // Signs contained within a block
+	Infos        *types.SwitchInfos   //change info
 }
 
 // blockBodiesData is the network packet for block content distribution.
