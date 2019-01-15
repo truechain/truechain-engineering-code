bf9c907daf68c841c71e007a3721d3f879dce609
wbqlove <wbqlove1234@163.com>
2019-01-15 11:55:58 +0800

    tbft node testing add map or not map multithreaded read and write

--- consensus/tbft/node_test.go
+++ consensus/tbft/node_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 type A struct {
@@ -28,4 +30,75 @@ func TestHex(t *testing.T) {
 	var c []byte
 	c = append(c, byte(222))
 	fmt.Println(common.ToHex(c), hexutil.Encode(c))
+}
+
+type Main struct {
+	sub map[int]string
+}
+
+type Main2 struct {
+	sub string
+}
+
+func GetRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+func TestStructMapNotLock(t *testing.T) {
+	out := make(chan bool)
+	tMain := &Main{sub: make(map[int]string)}
+
+	j := 0
+	for {
+		j++
+		if j > 100 {
+			break
+		}
+		go func() {
+			i := 0
+			for {
+				i++
+				if i > 1000 {
+					i = 0
+				}
+				tMain.sub[i] = GetRandomString(30)
+			}
+		}()
+	}
+	<-out
+}
+
+func TestStructNoMapNotLock(t *testing.T) {
+	out := make(chan bool)
+	tMain := &Main2{sub: ""}
+
+	j := 0
+	for {
+		j++
+		if j > 100 {
+			break
+		}
+		go func() {
+			i := 0
+			for {
+				i++
+				if i > 1000 {
+					i = 0
+				}
+				tMain.sub = GetRandomString(30)
+			}
+		}()
+	}
+	for {
+		time.Sleep(time.Second)
+		fmt.Println(tMain.sub)
+	}
+	<-out
 }
