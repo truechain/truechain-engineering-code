884c70b57922d96ee524b6ca9083927d9fc9d29c
tm <ice_ming@126.com>
2019-01-25 16:13:39 +0800

    watch.go: update watch for remove speed

--- consensus/tbft/help/watch.go
+++ consensus/tbft/help/watch.go
@@ -3,9 +3,10 @@ package help
 import (
 	// "fmt"
 	// "bytes"
-	"time"
 	"sync"
 	"sync/atomic"
+	"time"
+
 	// "github.com/ethereum/go-ethereum/crypto/sha3"
 	// "github.com/ethereum/go-ethereum/rlp"
 	// "github.com/ethereum/go-ethereum/common"
@@ -15,7 +16,7 @@ import (
 var (
 	watchs = newWatchMgr()
 	// WatchFinishTime used to handle the end event in every watch
-	WatchFinishTime = 10
+	WatchFinishTime = 3
 	// MaxLockTime	used to judge the watch is locked
 	MaxLockTime = 60
 	// MaxWatchInChan
@@ -37,73 +38,81 @@ func EndWatchMgr() {
 func WatchsCountInMgr() uint64 {
 	return watchs.count()
 }
+
 //-----------------------------------------------------------------------------
 // TWatch watch and output the cost time for exec
 type TWatch struct {
-	begin  		time.Time
-	end    		time.Time
-	expect 		float64
-	str    		string
-	ID			uint64
-	res 		chan uint64
-	endFlag 	uint32
+	begin   time.Time
+	end     time.Time
+	expect  float64
+	str     string
+	ID      uint64
+	res     chan uint64
+	endFlag uint32
 }
 
 // NewTWatch make the new watch
 func NewTWatch(e float64, s string) *TWatch {
 	w := &TWatch{
-		begin:  	time.Now(),
-		end:    	time.Now(),
-		expect: 	e,
-		str:    	s,
-		endFlag:	0,
+		begin:   time.Now(),
+		end:     time.Now(),
+		expect:  e,
+		str:     s,
+		endFlag: 0,
 	}
 	w.ID = watchs.getNewNum()
 	watchs.setWatch(w)
 	return w
 }
+
 // EndWatch end the watch
 func (in *TWatch) EndWatch() {
 	in.end = time.Now()
-	atomic.AddUint32(&in.endFlag,1)
+	atomic.AddUint32(&in.endFlag, 1)
 	select {
 	case watchs.getRes() <- in.ID:
 	default:
 		// go func() { watchs.getRes() <- in.ID }()
 	}
 }
+func (in *TWatch) endWatchNoNotify() {
+	in.end = time.Now()
+	atomic.AddUint32(&in.endFlag, 1)
+}
+
 func (in *TWatch) getEndFlag() bool {
 	v := atomic.LoadUint32(&in.endFlag)
 	return v > 0
 }
+
 // Finish count the cost time in watch
 func (in *TWatch) Finish(comment interface{}) {
 	if d := in.end.Sub(in.begin); d.Seconds() > in.expect {
-		log.Warn(in.str, "not expecting time", d.Seconds(), "comment", comment,"id",in.ID)
+		log.Warn(in.str, "not expecting time", d.Seconds(), "comment", comment, "id", in.ID)
 	}
 }
 
 type WatchMgr struct {
-	watchID		uint64
-	watchs		map[uint64]*TWatch
-	quit		bool
-	lock		*sync.Mutex
-	resFinish   chan uint64
+	watchID   uint64
+	watchs    map[uint64]*TWatch
+	quit      bool
+	lock      *sync.Mutex
+	resFinish chan uint64
 }
 
-func newWatchMgr() *WatchMgr {	
+func newWatchMgr() *WatchMgr {
 	w := &WatchMgr{
-		watchID:	0,
-		watchs:		make(map[uint64]*TWatch,0),
-		lock:		new(sync.Mutex),
-		resFinish:	make(chan uint64,MaxWatchInChan),
-		quit:		true,
+		watchID:   0,
+		watchs:    make(map[uint64]*TWatch, 0),
+		lock:      new(sync.Mutex),
+		resFinish: make(chan uint64, MaxWatchInChan),
+		quit:      true,
 	}
 	return w
 }
 
 func (w *WatchMgr) getNewNum() uint64 {
-	v := atomic.AddUint64(&w.watchID,1)
+	v := atomic.AddUint64(&w.watchID, 1)
 	return v
 }
 func (w *WatchMgr) start() {
@@ -117,14 +126,14 @@ func (w *WatchMgr) stop() {
 func (w *WatchMgr) setWatch(watch *TWatch) {
 	w.lock.Lock()
 	defer w.lock.Unlock()
-	if _,ok := w.watchs[watch.ID]; !ok {
+	if _, ok := w.watchs[watch.ID]; !ok {
 		w.watchs[watch.ID] = watch
 	}
 }
 func (w *WatchMgr) getWatch(id uint64) *TWatch {
 	w.lock.Lock()
 	defer w.lock.Unlock()
-	if v,ok := w.watchs[id]; ok {
+	if v, ok := w.watchs[id]; ok {
 		return v
 	}
 	return nil
@@ -132,57 +141,69 @@ func (w *WatchMgr) getWatch(id uint64) *TWatch {
 func (w *WatchMgr) remove(id uint64) {
 	w.lock.Lock()
 	defer w.lock.Unlock()
-	delete(w.watchs,id)
+	delete(w.watchs, id)
+}
+func (w *WatchMgr) removeByIDs(ids []uint64) {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	for _, v := range ids {
+		delete(w.watchs, v)
+	}
 }
 func (w *WatchMgr) count() uint64 {
 	w.lock.Lock()
 	defer w.lock.Unlock()
 	return uint64(len(w.watchs))
 }
-func (w *WatchMgr) getRes() chan<-uint64 {
+func (w *WatchMgr) getRes() chan<- uint64 {
 	return w.resFinish
 }
 func (w *WatchMgr) getLastWatchs() []*TWatch {
-	ws := make([]*TWatch,0,0)
+	ws := make([]*TWatch, 0, 0)
 	w.lock.Lock()
 	defer w.lock.Unlock()
-	for _,v := range w.watchs {
-		ws = append(ws,v)
+	for _, v := range w.watchs {
+		ws = append(ws, v)
 	}
 	return ws
 }
 func (w *WatchMgr) watchFinish() {
-	count := 0
+	ids := make([]uint64, 0, 0)
+	defer func() {
+		w.removeByIDs(ids)
+	}()
+
 	for {
 		select {
 		case id := <-w.resFinish:
-			w.remove(id)
-		case <-time.After(1*time.Second):
+			// w.remove(id)
+			ids = append(ids, id)
+		case <-time.After(1 * time.Second):
 			if w.quit {
-				return 
-			}
-			count++
-			if count >= WatchFinishTime {
 				return
 			}
+			return
 		}
 	}
 }
 func (w *WatchMgr) watchLoop() {
 	ws := w.getLastWatchs()
 	now := time.Now()
+	ids := make([]uint64, 0, 0)
+	defer func() {
+		w.removeByIDs(ids)
+	}()
 
-	for _,watch:= range ws {
-		if w.quit { return }
+	for _, watch := range ws {
+		if w.quit {
+			return
+		}
 		d := now.Sub(watch.begin)
 		end := watch.getEndFlag()
-		if !end && d.Seconds() > float64(MaxLockTime) && d.Seconds() > watch.expect * 10 {
-			go watch.EndWatch()
-			log.Warn("cost long time or forget endwatch","function",watch.str, "time", d.Seconds(),"id",watch.ID)
-		}
-		if end {
-			w.remove(watch.ID)
-			continue
+		if !end && d.Seconds() > float64(MaxLockTime) && d.Seconds() > watch.expect*10 {
+			watch.endWatchNoNotify()
+			ids = append(ids, watch.ID)
+			log.Warn("cost long time or forget endwatch", "function", watch.str, "time", d.Seconds(), "id", watch.ID)
 		}
 	}
 }
@@ -190,10 +211,12 @@ func (w *WatchMgr) watchLoop() {
 func (w *WatchMgr) work() {
 
 	for {
-		
-		if w.quit { return }
+
+		if w.quit {
+			return
+		}
 		w.watchFinish()
 		w.watchLoop()
-		time.Sleep(100 * time.Millisecond)	
+		time.Sleep(100 * time.Millisecond)
 	}
 }
