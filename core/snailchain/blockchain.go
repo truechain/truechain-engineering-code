e57248c54c48723cbd8edd063fe1d135feadd536
ywzqwwt <752252086@qq.com>
2019-02-26 15:54:57 +0800

    core/snailchain: update method HasBlockAndState

--- core/snailchain/blockchain.go
+++ core/snailchain/blockchain.go
@@ -549,6 +549,10 @@ func (bc *SnailBlockChain) HasState(hash common.Hash) bool {
 // HasBlockAndState checks if a block and associated state trie is fully present
 // in the database or not, caching it if present.
 func (bc *SnailBlockChain) HasBlockAndState(hash common.Hash, number uint64) bool {
+	//if get number bigger than currentNumber return nil
+	if number > bc.currentBlock.Load().(*types.SnailBlock).Number().Uint64() {
+		return false
+	}
 	// Check first that the block itself is known
 	block := bc.GetBlock(hash, number)
 	if block == nil {
