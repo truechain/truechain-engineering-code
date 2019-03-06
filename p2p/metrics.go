e47872c7a96fb03a7b804ec2d163e376e08aca89
jiyilanzhou <450595468@qq.com>
2019-03-06 12:05:41 +0800

    p2p : remove invaild watch

--- p2p/metrics.go
+++ p2p/metrics.go
@@ -20,7 +20,6 @@ package p2p
 
 import (
 	"fmt"
-	"github.com/truechain/truechain-engineering-code/consensus/tbft/help"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -134,7 +133,6 @@ func newMeteredConn(conn net.Conn, ingress bool, ip net.IP) net.Conn {
 // Read delegates a network read to the underlying connection, bumping the common
 // and the peer ingress traffic meters along the way.
 func (c *meteredConn) Read(b []byte) (n int, err error) {
-	watch := help.NewTWatch(3, fmt.Sprintf("ip: %s, tcp Send", c.RemoteAddr()))
 	n, err = c.Conn.Read(b)
 	ingressTrafficMeter.Mark(int64(n))
 	c.lock.RLock()
@@ -142,8 +140,6 @@ func (c *meteredConn) Read(b []byte) (n int, err error) {
 		c.ingressMeter.Mark(int64(n))
 	}
 	c.lock.RUnlock()
-	watch.EndWatch()
-	watch.Finish(fmt.Sprintf("end  n: %d err: %v ", n, err))
 	return n, err
 }
 
