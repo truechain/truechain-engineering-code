9a015b4d4339d2d283696f1c44811fae07b0cd2f
shuxun <shuxunyer@163.com>
2019-03-06 14:34:01 +0800

    pbft_Agent.go  remove log info

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -27,7 +27,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
