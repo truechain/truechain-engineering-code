157c6ce37bc87f99f0917f475b0391e054a5cd23
ywzqwwt <752252086@qq.com>
2019-03-20 11:16:35 +0800

    core/snailchain: add WriteHeadHeaderHash

--- core/snailchain/blockchain.go
+++ core/snailchain/blockchain.go
@@ -225,6 +225,7 @@ func (bc *SnailBlockChain) loadLastState() error {
 		maxFruitNumber = currentBlock.Fruits()[len(currentBlock.Fruits())-1].FastNumber()
 	}
 	rawdb.WriteHeadBlockHash(bc.db, currentBlock.Hash())
+	rawdb.WriteHeadHeaderHash(bc.db, currentBlock.Header().Hash())
 	rawdb.WriteHeadFastBlockHash(bc.db, bc.blockchain.CurrentBlock().Hash())
 	batch := bc.db.NewBatch()
 	for _, ft := range remove {
