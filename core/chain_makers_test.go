da0757e82d82f0cf04d88e4be4eb00717a992596
jiyilanzhou <450595468@qq.com>
2019-03-21 10:59:57 +0800

    core : fix chain_makers error

--- core/chain_makers_test.go
+++ core/chain_makers_test.go
@@ -17,8 +17,16 @@
 package core
 
 import (
+	"bytes"
+	"crypto/ecdsa"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
 	"math/big"
+	"os"
+	"runtime"
+	"testing"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/truechain/truechain-engineering-code/consensus/minerva"
@@ -105,49 +113,49 @@ func TestMakeBlock1(t *testing.T) {
 		addr2   = crypto.PubkeyToAddress(key2.PublicKey)
 		addr3   = crypto.PubkeyToAddress(key3.PublicKey)
 		addr4   = crypto.PubkeyToAddress(key4.PublicKey)
-		db      = ethdb.NewMemDatabase()
+		db      = etruedb.NewMemDatabase()
 	)
-	recvAddr := crypto.CreateAddress(common.Address{0},10)
+	recvAddr := crypto.CreateAddress(common.Address{0}, 10)
 	// Ensure that key1 has some funds in the genesis block.
 	gspec := &Genesis{
-		Config: &params.ChainConfig{HomesteadBlock: new(big.Int)},
+		Config:   &params.ChainConfig{ChainID: new(big.Int)},
 		GasLimit: 105000000,
-		Alloc:  GenesisAlloc{
+		Alloc: types.GenesisAlloc{
 			addr1: {Balance: big.NewInt(5000000000)},
 			addr2: {Balance: big.NewInt(5000000000)},
 			addr3: {Balance: big.NewInt(5000000000)},
 			addr4: {Balance: big.NewInt(5000000000)},
 		},
 	}
-	genesis := gspec.MustCommit(db)
+	genesis := gspec.MustFastCommit(db)
 
 	// This call generates a chain of 5 blocks. The function runs for
 	// each block and adds different features to gen based on the
 	// block index.
-	signer := types.HomesteadSigner{}
-	cnt := 4000/4
+	signer := types.MakeSigner(gspec.Config, nil)
+	cnt := 4000 / 4
 	finish := make(chan int)
 	type tmp struct {
 		addr common.Address
-		key *ecdsa.PrivateKey
+		key  *ecdsa.PrivateKey
 	}
-	addrs := []tmp {
-		{addr1,key1},
-		{addr2,key2},
-		{addr3,key3},
-		{addr4,key4},
+	addrs := []tmp{
+		{addr1, key1},
+		{addr2, key2},
+		{addr3, key3},
+		{addr4, key4},
 	}
 	begin := time.Now()
-	chain, _ := GenerateChain(gspec.Config, genesis, ethash.NewFaker(), db, 1, func(i int, gen *BlockGen) {
+	chain, _ := GenerateChain(gspec.Config, genesis, minerva.NewFaker(), db, 1, func(i int, gen *BlockGen) {
 		sum := 0
-		for i:=0;i<3;i++ {
-			go func(addr common.Address,key *ecdsa.PrivateKey){
-				for n:=0;n<cnt;n++ {
+		for i := 0; i < 3; i++ {
+			go func(addr common.Address, key *ecdsa.PrivateKey) {
+				for n := 0; n < cnt; n++ {
 					tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr), recvAddr, big.NewInt(1000), params.TxGas, nil, nil), signer, key)
 					gen.AddTx(tx)
 				}
 				finish <- 1
-			}(addrs[i].addr,addrs[i].key)
+			}(addrs[i].addr, addrs[i].key)
 		}
 		for {
 			<-finish
@@ -157,10 +165,10 @@ func TestMakeBlock1(t *testing.T) {
 			}
 		}
 	})
-	d := time.Now().Sub(begin);
-	fmt.Println("make block include", cnt, " txs,cost time ",d.Seconds())
+	d := time.Now().Sub(begin)
+	fmt.Println("make block include", cnt, " txs,cost time ", d.Seconds())
 	// Import the chain. This runs all block validation rules.
-	blockchain, _ := NewBlockChain(db, nil, gspec.Config, ethash.NewFaker(), vm.Config{}, nil)
+	blockchain, _ := NewBlockChain(db, nil, gspec.Config, minerva.NewFaker(), vm.Config{})
 	defer blockchain.Stop()
 
 	if i, err := blockchain.InsertChain(chain); err != nil {
@@ -184,163 +192,163 @@ func TestMakeBlock2(t *testing.T) {
 		addr2   = crypto.PubkeyToAddress(key2.PublicKey)
 		addr3   = crypto.PubkeyToAddress(key3.PublicKey)
 		addr4   = crypto.PubkeyToAddress(key4.PublicKey)
-		db      = ethdb.NewMemDatabase()
-		tmpDB   = ethdb.NewMemDatabase()
+		db      = etruedb.NewMemDatabase()
+		tmpDB   = etruedb.NewMemDatabase()
 	)
-	recvAddr := crypto.CreateAddress(common.Address{0},10)
-	recvAddr2 := crypto.CreateAddress(common.Address{0},50)
+	recvAddr := crypto.CreateAddress(common.Address{0}, 10)
+	recvAddr2 := crypto.CreateAddress(common.Address{0}, 50)
 	// Ensure that key1 has some funds in the genesis block.
 	gspec := &Genesis{
-		Config: &params.ChainConfig{HomesteadBlock: new(big.Int)},
+		Config:   &params.ChainConfig{ChainID: new(big.Int)},
 		GasLimit: 105000000,
-		Alloc:  GenesisAlloc{
+		Alloc: types.GenesisAlloc{
 			addr1: {Balance: big.NewInt(5000000000)},
 			addr2: {Balance: big.NewInt(5000000000)},
 			addr3: {Balance: big.NewInt(5000000000)},
 			addr4: {Balance: big.NewInt(5000000000)},
 		},
 	}
-	genesis := gspec.MustCommit(db)
-	genesis2 := gspec.MustCommit(tmpDB)
+	genesis := gspec.MustFastCommit(db)
+	genesis2 := gspec.MustFastCommit(tmpDB)
 
 	// This call generates a chain of 5 blocks. The function runs for
 	// each block and adds different features to gen based on the
 	// block index.
-	signer := types.HomesteadSigner{}
+	signer := types.MakeSigner(gspec.Config, nil)
 	cnt := 1
 	type tmp struct {
 		addr common.Address
-		key *ecdsa.PrivateKey
+		key  *ecdsa.PrivateKey
 	}
-	addrs := []tmp {
-		{addr1,key1},
-		{addr2,key2},
-		{addr3,key3},
-		{addr4,key4},
+	addrs := []tmp{
+		{addr1, key1},
+		{addr2, key2},
+		{addr3, key3},
+		{addr4, key4},
 	}
-	ptxs1 := make([]*types.Transaction,0,0)
-	ptxs2 := make([]*types.Transaction,0,0)
+	ptxs1 := make([]*types.Transaction, 0, 0)
+	ptxs2 := make([]*types.Transaction, 0, 0)
 	begin := time.Now()
-	block1, recpt1 := GenerateChain(gspec.Config, genesis, ethash.NewFaker(), db, 1, func(i int, gen *BlockGen) {
-		from,key := addrs[0].addr,addrs[0].key
-		for i:=0;i<cnt;i++ {
+	block1, recpt1 := GenerateChain(gspec.Config, genesis, minerva.NewFaker(), db, 1, func(i int, gen *BlockGen) {
+		from, key := addrs[0].addr, addrs[0].key
+		for i := 0; i < cnt; i++ {
 			tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(from), recvAddr, big.NewInt(1000), params.TxGas, nil, nil), signer, key)
-			ptxs1 = append(ptxs1,tx)
+			ptxs1 = append(ptxs1, tx)
 			gen.AddTx(tx)
 		}
-		from,key = addrs[1].addr,addrs[1].key
-		for i:=0;i<cnt;i++ {
+		from, key = addrs[1].addr, addrs[1].key
+		for i := 0; i < cnt; i++ {
 			tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(from), recvAddr2, big.NewInt(1000), params.TxGas, nil, nil), signer, key)
-			ptxs2 = append(ptxs2,tx)
+			ptxs2 = append(ptxs2, tx)
 			gen.AddTx(tx)
 		}
 	})
-	d := time.Now().Sub(begin);
-	fmt.Println("make block include", cnt, " txs,cost time ",d.Seconds())
+	d := time.Now().Sub(begin)
+	fmt.Println("make block include", cnt, " txs,cost time ", d.Seconds())
 	sum := len(ptxs1) + len(ptxs2)
-	indexs := make([]int,sum,sum)
+	indexs := make([]int, sum, sum)
 	// ptxs2 = append(ptxs1,ptxs2...)
 	// for i,_ := range ptxs2 {
 	// 	indexs[i] = i
 	// }
-	ptxs2 = append(ptxs2,ptxs1...)
+	ptxs2 = append(ptxs2, ptxs1...)
 	pos := 0
-	for i:=2*cnt-1;i>=cnt;i-- {
+	for i := 2*cnt - 1; i >= cnt; i-- {
 		indexs[pos] = i
 		pos++
 	}
-	for i:=cnt-1;i>=0;i-- {
+	for i := cnt - 1; i >= 0; i-- {
 		indexs[pos] = i
 		pos++
 	}
 	/////////////////////////////////////////////////////////////////////////////////
-	block2, recpt2 := GenerateChain(gspec.Config, genesis2, ethash.NewFaker(), tmpDB, 1, func(i int, gen *BlockGen) {
-		gen.AddTxWithChain2(nil,block1[0].Transactions(),ptxs2,indexs)
+	block2, recpt2 := GenerateChain(gspec.Config, genesis2, minerva.NewFaker(), tmpDB, 1, func(i int, gen *BlockGen) {
+		gen.AddTxWithChain2(nil, block1[0].Transactions(), ptxs2, indexs)
 	})
 	/////////////////////////////////////////////////////////////////////////////////
-	fmt.Println("r1.hash1:",recpt1[0].GetRlp(0),"r1.hash2",recpt1[0].GetRlp(1))
-	fmt.Println("r2.hash1:",recpt2[0].GetRlp(0),"r2.hash2",recpt2[0].GetRlp(1))
-	if bytes.Equal(recpt1[0].GetRlp(0),recpt2[0].GetRlp(0)) {
+	fmt.Println("r1.hash1:", recpt1[0].GetRlp(0), "r1.hash2", recpt1[0].GetRlp(1))
+	fmt.Println("r2.hash1:", recpt2[0].GetRlp(0), "r2.hash2", recpt2[0].GetRlp(1))
+	if bytes.Equal(recpt1[0].GetRlp(0), recpt2[0].GetRlp(0)) {
 		fmt.Println("recpt index 0 equal")
 	}
-	if bytes.Equal(recpt1[0].GetRlp(0),recpt2[0].GetRlp(1)) {
+	if bytes.Equal(recpt1[0].GetRlp(0), recpt2[0].GetRlp(1)) {
 		fmt.Println("recpt index 0---1 equal")
 	}
-	if bytes.Equal(recpt1[0].GetRlp(1),recpt2[0].GetRlp(0)) {
+	if bytes.Equal(recpt1[0].GetRlp(1), recpt2[0].GetRlp(0)) {
 		fmt.Println("recpt index 1---0 equal")
 	}
-	if bytes.Equal(recpt1[0].GetRlp(1),recpt2[0].GetRlp(1)) {
+	if bytes.Equal(recpt1[0].GetRlp(1), recpt2[0].GetRlp(1)) {
 		fmt.Println("recpt index 1 equal")
 	}
 
-	fmt.Println("height:",block1[0].NumberU64(),"hash1",block1[0].Hash(),"root:",block1[0].Root(),"rhash:",block1[0].ReceiptHash(),"txhash:",block1[0].TxHash())
-	fmt.Println("height:",block2[0].NumberU64(),"hash1",block2[0].Hash(),"root:",block2[0].Root(),"rhash:",block2[0].ReceiptHash(),"txhash:",block2[0].TxHash())
+	fmt.Println("height:", block1[0].NumberU64(), "hash1", block1[0].Hash(), "root:", block1[0].Root(), "rhash:", block1[0].ReceiptHash(), "txhash:", block1[0].TxHash())
+	fmt.Println("height:", block2[0].NumberU64(), "hash1", block2[0].Hash(), "root:", block2[0].Root(), "rhash:", block2[0].ReceiptHash(), "txhash:", block2[0].TxHash())
 
 	fmt.Println("finish")
 }
 func TestVerifySignInBatch(t *testing.T) {
-	fmt.Println("begin......,cpu",runtime.NumCPU())
-	verifyInBatch(100,1)
-	verifyInBatch(100,4)
-	verifyInBatch(100,10)
-	verifyInBatch(10000,1)
-	verifyInBatch(10000,4)
-	verifyInBatch(10000,10)
+	fmt.Println("begin......,cpu", runtime.NumCPU())
+	verifyInBatch(100, 1)
+	verifyInBatch(100, 4)
+	verifyInBatch(100, 10)
+	verifyInBatch(10000, 1)
+	verifyInBatch(10000, 4)
+	verifyInBatch(10000, 10)
 	fmt.Println("finish.....")
 }
 
-func verifyInBatch(sum,count int) {
+func verifyInBatch(sum, count int) {
 	var (
-		db      = ethdb.NewMemDatabase()
+		db = etruedb.NewMemDatabase()
 	)
 	type tmpItem struct {
-		sign 	[]byte
-		priv	*ecdsa.PrivateKey
-		hash 	common.Hash
+		sign []byte
+		priv *ecdsa.PrivateKey
+		hash common.Hash
 	}
 	gspec := &Genesis{
-		Config: &params.ChainConfig{HomesteadBlock: new(big.Int)},
+		Config:   &params.ChainConfig{ChainID: new(big.Int)},
 		GasLimit: 105000000,
-		Alloc:  GenesisAlloc{},
+		Alloc:    types.GenesisAlloc{},
 	}
-	genesis := gspec.MustCommit(db)
-	block1, _ := GenerateChain(gspec.Config, genesis, ethash.NewFaker(), db, 1, func(i int, gen *BlockGen) {
+	genesis := gspec.MustFastCommit(db)
+	block1, _ := GenerateChain(gspec.Config, genesis, minerva.NewFaker(), db, 1, func(i int, gen *BlockGen) {
 	})
 	block := block1[0]
 	hash := block.Hash()
 
-	items := make([]*tmpItem,sum,sum)
-	for i:=0;i<sum;i++ {
-		p,_ := crypto.GenerateKey()
-		sign,_ := crypto.Sign(hash[:],p)
+	items := make([]*tmpItem, sum, sum)
+	for i := 0; i < sum; i++ {
+		p, _ := crypto.GenerateKey()
+		sign, _ := crypto.Sign(hash[:], p)
 		items[i] = &tmpItem{
-			sign:		sign,
-			priv:		p,
-			hash:		hash,
+			sign: sign,
+			priv: p,
+			hash: hash,
 		}
 	}
 
-	finish := make(chan bool,count)
+	finish := make(chan bool, count)
 	fmt.Println("begin verify in batch...")
 	begin := time.Now()
 
-	execFunc := func(index int,infos []*tmpItem){
+	execFunc := func(index int, infos []*tmpItem) {
 		taskbegin := time.Now()
-		for _,v := range infos {
-			crypto.SigToPub(v.hash[:],v.sign)
+		for _, v := range infos {
+			crypto.SigToPub(v.hash[:], v.sign)
 		}
-		d := time.Now().Sub(taskbegin);
-		finish<-true
-		fmt.Println("index",index,"verify sign result, cost time: ",d.Seconds())
+		d := time.Now().Sub(taskbegin)
+		finish <- true
+		fmt.Println("index", index, "verify sign result, cost time: ", d.Seconds())
 	}
 	batch := sum / count
-	for i:=0;i<count;i++ {
-		b,e := i*batch,(i+1)*batch-1
-		if i == count -1 {
+	for i := 0; i < count; i++ {
+		b, e := i*batch, (i+1)*batch-1
+		if i == count-1 {
 			e = sum
 		}
 		info := items[b:e]
-		go execFunc(i,info)
+		go execFunc(i, info)
 	}
 
 	calcCount := 0
@@ -351,20 +359,23 @@ func verifyInBatch(sum,count int) {
 			break
 		}
 	}
-	d := time.Now().Sub(begin);
-	fmt.Println("all task done, sign Sum:",sum,"task count:",count,"cost time: ",d.Seconds())
+	d := time.Now().Sub(begin)
+	fmt.Println("all task done, sign Sum:", sum, "task count:", count, "cost time: ", d.Seconds())
 }
+
 // for test
-func (b *BlockGen) AddTxWithChain2(bc *BlockChain, otxs,txs []*types.Transaction,indexs []int) {
+func (b *BlockGen) AddTxWithChain2(bc *BlockChain, otxs, txs []*types.Transaction, indexs []int) {
 	if b.gasPool == nil {
 		b.SetCoinbase(common.Address{})
 	}
 
-	recpts := make([]*types.Receipt,len(otxs),len(otxs))
+	recpts := make([]*types.Receipt, len(otxs), len(otxs))
 
-	for i,tx := range txs {
+	for i, tx := range txs {
 		b.statedb.Prepare(tx.Hash(), common.Hash{}, indexs[i])
-		receipt, _, err := ApplyTransaction(b.config, bc, &b.header.Coinbase, b.gasPool, b.statedb, b.header, tx, &b.header.GasUsed, vm.Config{})
+
+		feeAmount := big.NewInt(0)
+		receipt, _, err := ApplyTransaction(b.config, bc, b.gasPool, b.statedb, b.header, tx, &b.header.GasUsed, feeAmount, vm.Config{})
 		if err != nil {
 			panic(err)
 		}
