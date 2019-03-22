9858391dae8797181f24e84dd3dcfb971dbc7a53
ywzqwwt <752252086@qq.com>
2019-03-22 13:54:18 +0800

    core/snailchain: del struct CacheConfig

--- core/snailchain/blockchain.go
+++ core/snailchain/blockchain.go
@@ -61,14 +61,6 @@ const (
 	badBlockLimit       = 10
 )
 
-// CacheConfig contains the configuration values for the trie caching/pruning
-// that's resident in a blockchain.
-type CacheConfig struct {
-	Disabled      bool          // Whether to disable trie write caching (archive node)
-	TrieNodeLimit int           // Memory limit (MB) at which to flush the current in-memory trie to disk
-	TrieTimeLimit time.Duration // Time limit after which to flush the current in-memory trie to disk
-}
-
 // SnailBlockChain represents the canonical chain given a database with a genesis
 // block. The Blockchain manages chain imports, reverts, chain reorganisations.
 //
