8f6d4c5fea1b651c820529091e79f39c2a3f6462
ywzqwwt <752252086@qq.com>
2019-04-19 11:56:29 +0800

    core/snailchain: update loadLastState

--- core/snailchain/blockchain.go
+++ core/snailchain/blockchain.go
@@ -189,7 +189,7 @@ func (bc *SnailBlockChain) loadLastState() error {
 	}
 	remove := make(types.Fruits, 0, len(currentBlock.Fruits()))
 	maxFruitNumber := currentBlock.Fruits()[len(currentBlock.Fruits())-1].FastNumber()
-	for maxFruitNumber != nil && maxFruitNumber.Cmp(bc.blockchain.CurrentBlock().Number()) > 0 {
+	for maxFruitNumber != nil && maxFruitNumber.Cmp(bc.blockchain.CurrentHeader().Number) > 0 {
 		log.Debug("rollback snailBlock", "snailBlock number", currentBlock.Number(), "maxFruitNumber", maxFruitNumber, "current fastblock number", bc.blockchain.CurrentBlock().Number())
 		parentHash := currentBlock.ParentHash()
 		for _, ft := range currentBlock.Fruits() {
