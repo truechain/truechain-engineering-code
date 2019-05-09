f09a3e329a22de5eb4a31f57b4d31f311ff96821
shuxun <shuxunyer@163.com>
2019-05-09 19:58:29 +0800

    params/config.go addTip12

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -425,7 +425,8 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	// When an error was returned by the EVM or when setting the creation code
 	// above we revert to the snapshot and consume any gas remaining. Additionally
 	// when we're in homestead this also counts for code storage gas errors.
-	if maxCodeSizeExceeded || err != nil /*&& err != ErrCodeStoreOutOfGas*/ {
+	//if maxCodeSizeExceeded || err != nil && err != ErrCodeStoreOutOfGas {
+	if maxCodeSizeExceeded || (err != nil && (evm.ChainConfig().IsTIP12(evm.BlockNumber) || err != ErrCodeStoreOutOfGas)) {
 		evm.StateDB.RevertToSnapshot(snapshot)
 		if err != errExecutionReverted {
 			contract.UseGas(contract.Gas)
