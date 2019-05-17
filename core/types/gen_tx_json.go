a507e1cd8c49d50388a9216c64a653ced60a13f9
shuxun <shuxunyer@163.com>
2019-05-17 16:46:40 +0800

    core/types/gen_Tx_json.go remove rlp"-"

--- core/types/gen_tx_json.go
+++ core/types/gen_tx_json.go
@@ -22,7 +22,7 @@ func (t txdata) MarshalJSON() ([]byte, error) {
 		Amount       *hexutil.Big    `json:"value"    gencodec:"required"`
 		Payload      hexutil.Bytes   `json:"input"    gencodec:"required"`
 		Payer        *common.Address `json:"payer"    rlp:"nil"`
-		Fee          *hexutil.Big    `json:"fee"    	rlp:"nil"`
+		Fee          *hexutil.Big    `json:"fee" rlp:"nil"`
 		V            *hexutil.Big    `json:"v" gencodec:"required"`
 		R            *hexutil.Big    `json:"r" gencodec:"required"`
 		S            *hexutil.Big    `json:"s" gencodec:"required"`
