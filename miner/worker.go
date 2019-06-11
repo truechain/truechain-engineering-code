875247f8a404e160914ba69f3808060b1a9ea9c4
tm <ice_ming@126.com>
2019-06-11 16:51:05 +0800

    miner: update MinTimeGap for compatible in temporary

--- miner/worker.go
+++ miner/worker.go
@@ -771,8 +771,8 @@ func (w *worker) CommitFruits(fruits []*types.SnailBlock, bc *chain.SnailBlockCh
 
 			unmineFruitLen := new(big.Int).Sub(fastHight, fruits[len(fruits)-1].FastNumber())
 			waitmine := rand.Intn(900)
-
-			if timeinterval.Cmp(params.MinTimeGap) >= 0 && (waitmine > int(unmineFruitLen.Int64())) {
+			tmp := big.NewInt(360)			// upgrade for temporary
+			if timeinterval.Cmp(tmp) >= 0 && (waitmine > int(unmineFruitLen.Int64())) {
 				// must big then 5min
 				w.current.fruits = fruitset
 			} else {
