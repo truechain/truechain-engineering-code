0427c6982aa614e84d44851c4bc4428df832a723
shijinyuan <13152720003@163.com>
2019-06-12 16:00:34 +0800

    core：update downloader add log

--- etrue/downloader/downloader.go
+++ etrue/downloader/downloader.go
@@ -1416,6 +1416,8 @@ func (d *Downloader) importBlockResults(results []*etrue.FetchResult, p etrue.Pe
 
 	maxSize := maxSyncSnailHeight
 	txLen := len(sblocks)
+
+	log.Info("Snail insert download","txLen",txLen)
 	if txLen > maxSize {
 		for i := 0; i < txLen; {
 			i = i + maxSize
