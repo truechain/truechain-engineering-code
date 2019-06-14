3185f003cbbf1b40a2219cfae165b5069797e19a
shijinyuan <13152720003@163.com>
2019-06-14 17:05:25 +0800

    etrue/downloader:update SnapShotSync

--- etrue/sync.go
+++ etrue/sync.go
@@ -318,9 +318,11 @@ func (pm *ProtocolManager) synchronise(peer *peer) {
 	if atomic.LoadUint32(&pm.fastSync) == 1 {
 		// Fast sync was explicitly requested, and explicitly granted
 		mode = downloader.FastSync
-	} else if atomic.LoadUint32(&pm.snapSync) == 1 {
-		mode = downloader.SnapShotSync
-	} else if pm.blockchain.CurrentBlock().NumberU64() == 0 && pm.blockchain.CurrentFastBlock().NumberU64() > 0 {
+
+		//else if atomic.LoadUint32(&pm.snapSync) == 1 {
+		//	mode = downloader.SnapShotSync
+		//}
+	}else if pm.blockchain.CurrentBlock().NumberU64() == 0 && pm.blockchain.CurrentFastBlock().NumberU64() > 0 {
 		// The database  seems empty as the current block is the genesis. Yet the fast
 		// block is ahead, so fast sync was enabled for this node at a certain point.
 		// The only scenario where this can happen is if the user manually (or via a
