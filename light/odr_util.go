22a42dd231ec355967f3c44fd51d7772d02550c4
jiyilanzhou <450595468@qq.com>
2019-06-17 14:40:44 +0800

    les : fix log

--- light/odr_util.go
+++ light/odr_util.go
@@ -19,7 +19,7 @@ package light
 import (
 	"bytes"
 	"context"
-	"github.com/ethereum/go-ethereum/swarm/log"
+	"github.com/ethereum/go-ethereum/log"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
