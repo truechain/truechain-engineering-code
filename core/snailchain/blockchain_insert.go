f530644fbb4dfc6e4bb25e4ef8793f5b04f08bff
ywzqwwt <752252086@qq.com>
2019-07-12 18:27:19 +0800

    core/snailchain: update verify order

--- core/snailchain/blockchain_insert.go
+++ core/snailchain/blockchain_insert.go
@@ -98,9 +98,9 @@ func (it *insertIterator) next(verifyFruits bool) (*types.SnailBlock, error) {
 	if err := <-it.results; err != nil {
 		return it.chain[it.index], err
 	}
-	err := it.validator.ValidateBody(it.chain[it.index], verifyFruits)
+	err := it.validator.ValidateRewarded(it.chain[it.index].NumberU64())
 	if err == nil {
-		err = it.validator.ValidateRewarded(it.chain[it.index].NumberU64())
+		err = it.validator.ValidateBody(it.chain[it.index], verifyFruits)
 	}
 	return it.chain[it.index], err
 }
