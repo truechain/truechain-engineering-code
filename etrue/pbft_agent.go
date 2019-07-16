94ba169eb2546e9829eaafb584b1bb6ea3a9274c
ywzqwwt <752252086@qq.com>
2019-07-16 18:08:33 +0800

    etrue/pbft_agent.go: change slice initial length to 0

--- etrue/pbft_agent.go
+++ etrue/pbft_agent.go
@@ -1322,7 +1322,7 @@ func (agent *PbftAgent) singleloop() {
 //GetCurrentCommittee return committee member's pubkey information
 func (agent *PbftAgent) GetCurrentCommittee() []string {
 	members := agent.currentCommitteeInfo.Members
-	memberKeys := make([]string, len(members))
+	memberKeys := make([]string, 0)
 	for _, member := range members {
 		pubKeyStr := fmt.Sprintf("%x", member.Publickey)
 		memberKeys = append(memberKeys, pubKeyStr)
@@ -1333,7 +1333,7 @@ func (agent *PbftAgent) GetCurrentCommittee() []string {
 //GetAlternativeCommittee return received back committee member's pubkey information
 func (agent *PbftAgent) GetAlternativeCommittee() []string {
 	members := agent.currentCommitteeInfo.BackMembers
-	memberKeys := make([]string, len(members))
+	memberKeys := make([]string, 0)
 	for _, member := range members {
 		pubKeyStr := fmt.Sprintf("%x", member.Publickey)
 		memberKeys = append(memberKeys, pubKeyStr)
