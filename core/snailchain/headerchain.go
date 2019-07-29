a8a8972c15073005ee22589e6caa938cf385b49f
ywzqwwt <752252086@qq.com>
2019-07-29 11:39:23 +0800

    core/snailchain: update ValidateHeaderChain,change return fruit idex to block index

--- core/snailchain/headerchain.go
+++ core/snailchain/headerchain.go
@@ -292,7 +292,7 @@ func (hc *HeaderChain) ValidateHeaderChain(chain []*types.SnailHeader, fruits []
 		for j, fruit := range fruitHeaders {
 			if err := hc.engine.ValidateFruitHeader(header, fruit, hc, fastchain, checkpoint); err != nil {
 				log.Error("ValidateHeaderChain", "snailBlock number", header.Number, "fruit number", fruit.FastNumber, "err", err)
-				return j, err
+				return i, err
 			}
 		}
 	}
