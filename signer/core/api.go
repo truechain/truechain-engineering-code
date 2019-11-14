7109cafa5374f67dddf9c10e94293bacbdf58816
lesterli <lester123@live.cn>
2019-11-14 10:56:31 +0800

    signer: add smart card hub

--- signer/core/api.go
+++ signer/core/api.go
@@ -23,8 +23,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/big"
+	"os"
 	"reflect"
 
+	"github.com/ethereum/go-ethereum/accounts/scwallet"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -129,6 +131,25 @@ func StartClefAccountManager(ksLocation string, nousb, lightKDF bool, scpath str
 		} else {
 			backends = append(backends, trezorhub)
 			log.Debug("Trezor support enabled via WebUSB")
+		}
+	}
+
+	// Start a smart card hub
+	if len(scpath) > 0 {
+		// Sanity check that the smartcard path is valid
+		fi, err := os.Stat(scpath)
+		if err != nil {
+			log.Info("Smartcard socket file missing, disabling", "err", err)
+		} else {
+			if fi.Mode()&os.ModeType != os.ModeSocket {
+				log.Error("Invalid smartcard socket file type", "path", scpath, "type", fi.Mode().String())
+			} else {
+				if schub, err := scwallet.NewHub(scpath, scwallet.Scheme, ksLocation); err != nil {
+					log.Warn(fmt.Sprintf("Failed to start smart card hub, disabling: %v", err))
+				} else {
+					backends = append(backends, schub)
+				}
+			}
 		}
 	}
 
