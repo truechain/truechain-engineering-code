92e39cd3c043c64fd6f9ca31f7b418beb627bd7a
lesterli <lester123@live.cn>
2019-12-09 11:41:00 +0800

    add rlp directory

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -28,9 +28,9 @@ import (
 	"math/big"
 	"os"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/truechain/truechain-engineering-code/common"
+	"github.com/truechain/truechain-engineering-code/common/math"
+	"github.com/truechain/truechain-engineering-code/rlp"
 	"golang.org/x/crypto/sha3"
 )
 
