e219ab7df3ecc5f3ed0329f8fd20e9973643c4cb
jiyilanzhou <450595468@qq.com>
2020-01-04 18:37:16 +0800

    core/genesis : genesis AddBalance stake

--- core/genesis.go
+++ core/genesis.go
@@ -268,6 +268,7 @@ func (g *Genesis) ToFastBlock(db etruedb.Database) *types.Block {
 			if err != nil {
 				log.Error("ToFastBlock InsertSAccount", "error", err)
 			}
+			statedb.AddBalance(member.Coinbase, big.NewInt(100000))
 		}
 		_, err := impl.DoElections(1, 0)
 		if err != nil {
