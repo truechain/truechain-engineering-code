f34f7cffbc6277f21ab7159d4e01f713147bf9c2
jiyilanzhou <450595468@qq.com>
2020-01-11 11:14:46 +0800

    test/pos_test: add GetLockedAsset test

--- send_transaction/test/pos_test.go
+++ send_transaction/test/pos_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/truechain/truechain-engineering-code/core"
 	"github.com/truechain/truechain-engineering-code/core/types"
+	"github.com/truechain/truechain-engineering-code/core/vm"
 	"github.com/truechain/truechain-engineering-code/log"
 	"math/big"
 	"os"
@@ -22,6 +23,30 @@ func TestOnlyDeposit(t *testing.T) {
 
 		sendDepositTransaction(number, gen, saddr1, skey1, signer, fastChain, abiStaking, nil, header)
 		sendCancelTransaction(number, gen, saddr1, big.NewInt(1000000000000000000), skey1, signer, fastChain, abiStaking, nil, header)
+		sendWithdrawTransaction(number, gen, saddr1, big.NewInt(1000000000000000000), skey1, signer, fastChain, abiStaking, nil, header)
+	}
+	manager := newTestPOSManager(40, executable)
+	fmt.Println(" saddr1 ", manager.GetBalance(saddr1))
+}
+
+///////////////////////////////////////////////////////////////////////
+func TestGetLockedAsset(t *testing.T) {
+	// Create a helper to check if a gas allowance results in an executable transaction
+	executable := func(number uint64, gen *core.BlockGen, fastChain *core.BlockChain, header *types.Header) {
+		snedTranction(number, gen, fastChain, mAccount, saddr1, big.NewInt(6000000000000000000), priKey, signer, nil, header)
+
+		sendDepositTransaction(number, gen, saddr1, skey1, signer, fastChain, abiStaking, nil, header)
+		sendCancelTransaction(number, gen, saddr1, big.NewInt(1000000000000000000), skey1, signer, fastChain, abiStaking, nil, header)
+		if number == 90 {
+			stateDb := gen.GetStateDB()
+			impawn := vm.NewImpawnImpl()
+			impawn.Load(stateDb, vm.StakingAddress)
+			arr := impawn.GetLockedAsset(daddr1)
+			for addr, value := range arr {
+				fmt.Println("value ", value.Value, " addr ", addr.String())
+			}
+		}
+
 		sendWithdrawTransaction(number, gen, saddr1, big.NewInt(1000000000000000000), skey1, signer, fastChain, abiStaking, nil, header)
 	}
 	manager := newTestPOSManager(40, executable)
