82d9fe7084d9476577c4f846dabd4d51874f13f4
iceming123 <ice_ming@126.com>
2020-02-10 17:38:45 +0800

    genesis_test.go: add test for explicit result of sa/da reward

--- core/genesis_test.go
+++ core/genesis_test.go
@@ -20,7 +20,9 @@ package core
 import (
 	"math/big"
 	"reflect"
+	"sort"
 	"testing"
+	"bytes"
 	"fmt"
 	"encoding/hex"
 	"github.com/davecgh/go-spew/spew"
@@ -299,9 +301,11 @@ var (
 	key1 = "da5756ffa265ed55dcb741c97e8d3d2f36269df8afcae4b59b0b1f1f8eb58977"
 	addr1 = "0x573baF2a36BFd683F1301db1EeBa1D55fd14De0A"
 	balance1 = new(big.Int).Mul(big.NewInt(1000),big.NewInt(1e18))
+	balance2 = new(big.Int).Mul(big.NewInt(10000),big.NewInt(1e18))
 	gp       = new(GasPool).AddGas(new(big.Int).Mul(big.NewInt(1),big.NewInt(1e18)).Uint64())
 	code = `0x608060405234801561001057600080fd5b506040516020806101758339810180604052810190808051906020019092919050505060006a747275657374616b696e6790508073ffffffffffffffffffffffffffffffffffffffff1663e1254fba836040518263ffffffff167c0100000000000000000000000000000000000000000000000000000000028152600401808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001915050606060405180830381600087803b1580156100de57600080fd5b505af11580156100f2573d6000803e3d6000fd5b505050506040513d606081101561010857600080fd5b8101908080519060200190929190805190602001909291908051906020019092919050505050505050506035806101406000396000f3006080604052600080fd00a165627a7a72305820a76679c2a9c73eeafffe41cfccde51b6b5150b920f6d90f25792987d9ab855c400290000000000000000000000006d348e0188cc2596aaa4046a1d50bb3ba50e8524`
 	gasLimit = uint64(3000000)
+	allReward = new(big.Int).Mul(big.NewInt(100),big.NewInt(1e18))
 )
 
 func getFisrtState() *state.StateDB {
@@ -315,14 +319,16 @@ func TestTip7(t *testing.T) {
 
 	fmt.Println("finish")
 }
-func generateAddr() {
+func generateAddr() common.Address {
 	priv,_ := crypto.GenerateKey()
 	privHex := hex.EncodeToString(crypto.FromECDSA(priv))
 	fmt.Println(privHex)
 	addr := crypto.PubkeyToAddress(priv.PublicKey)
 	fmt.Println(addr.String())
 	fmt.Println("finish")
+	return addr
 }
+
 func toFirstBlock(statedb *state.StateDB)  {
 	statedb.AddBalance(common.HexToAddress(addr1),balance1)
 	config := params.DevnetChainConfig
@@ -381,4 +387,198 @@ func TestDeployedTx(t *testing.T) {
 	fmt.Printf("last block: #%d\n", blockchain.CurrentBlock().Number())
 	fmt.Println("balance of addr1:", state.GetBalance(addr1))
 	fmt.Println("finish")
+}
+type da struct {
+	address 	common.Address
+	amount 		*big.Int
+	reward 		*big.Int
+}
+type daByAmount []*da
+func (vs daByAmount) Len() int {
+	return len(vs)
+}
+func (vs daByAmount) Less(i, j int) bool {
+	return vs[i].amount.Cmp(vs[j].amount) < 0 
+}
+func (vs daByAmount) Swap(i, j int) {
+	it := vs[i]
+	vs[i] = vs[j]
+	vs[j] = it
+}
+
+type sa struct {
+	address 	common.Address
+	fee 		*big.Int
+	amount 		*big.Int
+	reward 		*big.Int
+	das 		[]*da
+	pk 			[]byte
+}
+
+func (s *sa) getAllAmount() *big.Int {
+	amount := new(big.Int).Set(s.amount)
+	for _,v := range s.das {
+		amount = amount.Add(amount,v.amount)
+	}
+	return amount
+}
+func (s *sa) getAmount() *big.Int {
+	return s.amount
+}
+
+type saByAmount []*sa
+func (vs saByAmount) Len() int {
+	return len(vs)
+}
+func (vs saByAmount) Less(i, j int) bool {
+	return vs[i].getAllAmount().Cmp(vs[j].getAllAmount()) < 0 
+}
+func (vs saByAmount) Swap(i, j int) {
+	it := vs[i]
+	vs[i] = vs[j]
+	vs[j] = it
+}
+func (vs saByAmount) getAllAmount() *big.Int {
+	amount := big.NewInt(0)
+	for _,v := range vs {
+		amount = amount.Add(amount,v.getAllAmount())
+	}
+	return amount
+}
+
+func TestReward(t *testing.T) {
+	want := uint64(100)
+	params.DposForkPoint = want
+	key0,_ := crypto.GenerateKey()
+	pk := crypto.FromECDSAPub(&key0.PublicKey)
+
+	var (
+		accounts = []*sa {
+			&sa{
+				address:	generateAddr(),
+				fee:		big.NewInt(20),
+				amount:		balance2,
+				reward:		big.NewInt(0),
+				pk:			pk,
+				das:		[]*da{
+					&da{
+						address:	generateAddr(),
+						amount:		balance1,
+						reward:		big.NewInt(0),
+					},
+					&da{
+						address:	generateAddr(),
+						amount:		balance1,
+						reward:		big.NewInt(0),
+					},
+				},
+			},
+			&sa{
+				address:	generateAddr(),
+				fee:		big.NewInt(20),
+				amount:		balance2,
+				reward:		big.NewInt(0),
+				pk:			pk,
+				das:		[]*da{
+					&da{
+						address:	generateAddr(),
+						amount:		balance1,
+						reward:		big.NewInt(0),
+					},
+					&da{
+						address:	generateAddr(),
+						amount:		balance1,
+						reward:		big.NewInt(0),
+					},
+				},
+			},
+		}
+	)
+
+	impl := vm.NewImpawnImpl()
+	for _,val := range accounts {
+		impl.InsertSAccount2(want, val.address, val.pk, val.amount, val.fee, true)
+		for _,val2 := range val.das {
+			impl.InsertDAccount2(want, val.address, val2.address, val2.amount)
+		}
+	}
+	
+	_, err := impl.DoElections(1, want)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rinfo, _ := impl.Reward2(0,want, allReward)
+	
+	for i,v := range accounts {
+		wReward := getReward(v.address,rinfo)
+		if wReward.Sign() > 0 && wReward.Cmp(v.reward) != 0 {
+			fmt.Println("i:",i,"sa reward not match")
+		}
+		for ii,vv := range v.das {
+			wReward2 := getReward(vv.address,rinfo)
+			if wReward2.Sign() > 0 && wReward2.Cmp(vv.reward) != 0 {
+				fmt.Println("i:",i,"j",ii,"da reward not match")
+			}
+		}
+	}
+	fmt.Println("finish")
+}
+func calcReward(accounts []*sa) {
+	sas := make([]*sa,0,0)
+	for _,v := range accounts {
+		if params.ElectionMinLimitForStaking.Cmp(v.getAmount()) <= 0 {
+			sas = append(sas,v)
+		}
+	}
+	calcReward2(sas,allReward)
+}
+func calcReward2(accounts []*sa,allReward *big.Int) {
+	sort.Sort(saByAmount(accounts))
+	allStaking := saByAmount(accounts).getAllAmount()
+	sum := len(accounts)
+	left := big.NewInt(0)
+
+	for i,v := range accounts {
+		saAllStaking := v.getAllAmount()
+		if saAllStaking.Sign() <= 0 {
+			continue
+		}
+
+		v2 := new(big.Int).Quo(new(big.Int).Mul(saAllStaking, allReward), allStaking)
+		if i == sum-1 {
+			v2 = new(big.Int).Sub(allReward, left)
+		}
+		left = left.Add(left, v2)
+
+		calcRewardInSa(v, v2)
+	}
+}
+func calcRewardInSa(aa *sa,allReward *big.Int) {
+	allStaking := aa.getAllAmount()
+	fee := new(big.Int).Quo(new(big.Int).Mul(allReward, aa.fee), types.Base)
+	all, left := new(big.Int).Sub(allReward, fee), big.NewInt(0)
+	sort.Sort(daByAmount(aa.das))
+
+	for _, v := range aa.das {
+		daAll := v.amount
+		if daAll.Sign() <= 0 {
+			continue
+		}
+		v1 := new(big.Int).Quo(new(big.Int).Mul(all, daAll), allStaking)
+		left = left.Add(left, v1)
+		v.reward = v1
+	}
+	aa.reward = new(big.Int).Add(new(big.Int).Sub(all, left), fee)
+}
+func getReward(addr common.Address,infos []*types.SARewardInfos) *big.Int {
+	reward := big.NewInt(0)
+	for _,v := range infos {
+		for _,vv := range v.Items {
+			if bytes.Equal(vv.Address.Bytes(), addr.Bytes()) {
+				return vv.Amount
+			}
+		}
+	}
+	return reward
 }
