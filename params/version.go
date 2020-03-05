fb4596d434d6578b2bcc9a615575970124c8c418
iceming123 <ice_ming@126.com>
2020-03-05 19:14:38 +0800

    version.go: update stable version

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 2          // Major version component of the current release
 	VersionMinor = 0          // Minor version component of the current release
 	VersionPatch = 0          // Patch version component of the current release
-	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
