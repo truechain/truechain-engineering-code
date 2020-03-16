11d1e873b0d35f6d3c33780bc4046d103e9d893e
iceming123 <ice_ming@126.com>
2020-03-16 09:54:38 +0800

    tbft_config.go: update timeout of tbft

--- params/tbft_config.go
+++ params/tbft_config.go
@@ -249,7 +249,7 @@ func DefaultConsensusConfig() *ConsensusConfig {
 		TimeoutPrevoteDelta:         500,
 		TimeoutPrecommit:            10000,
 		TimeoutPrecommitDelta:       2000,
-		TimeoutCommit:               2000,
+		TimeoutCommit:               4500,
 		TimeoutCatchup:              1000,
 		SkipTimeoutCommit:           false,
 		CreateEmptyBlocks:           true,
