1e3612779ae536fd2bccba4a043e4b8be423594a
LIUYang <hyangl@163.com>
2021-07-25 12:07:05 +0800

    vendor: update vendor.

---
+++ vendor/github.com/holiman/bloomfilter/v2/conformance.go
@@ -0,0 +1,35 @@
+// Package bloomfilter is face-meltingly fast, thread-safe,
+// marshalable, unionable, probability- and
+// optimal-size-calculating Bloom filter in go
+//
+// https://github.com/holiman/bloomfilter
+//
+// Original source:
+// https://github.com/steakknife/bloomfilter
+//
+// Copyright © 2014, 2015, 2018 Barry Allard
+// Copyright © 2020 Martin Holst Swende
+//
+// MIT license
+//
+
+package v2
+
+import (
+	"encoding"
+	"encoding/gob"
+	"encoding/json"
+	"io"
+)
+
+// compile-time conformance tests
+var (
+	_ encoding.BinaryMarshaler   = (*Filter)(nil)
+	_ encoding.BinaryUnmarshaler = (*Filter)(nil)
+	_ io.ReaderFrom              = (*Filter)(nil)
+	_ io.WriterTo                = (*Filter)(nil)
+	_ gob.GobDecoder             = (*Filter)(nil)
+	_ gob.GobEncoder             = (*Filter)(nil)
+	_ json.Marshaler             = (*Filter)(nil)
+	_ json.Unmarshaler           = (*Filter)(nil)
+)
